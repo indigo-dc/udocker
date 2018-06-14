019e4d663d226aaa0caa3b173da6d1ce8264a0ab
jorge <jorge@lip.pt>
2018-06-14 15:01:08 +0100

    add nvidia driver support setup --nvidia

--- udocker.py
+++ udocker.py
@@ -30,6 +30,7 @@ import time
 import pwd
 import grp
 import platform
+import glob
 
 __author__ = "udocker@lip.pt"
 __credits__ = ["PRoot http://proot.me",
@@ -177,8 +178,7 @@ class Config(object):
 
     # directories to be mapped in contaners with: run --sysdirs
     sysdirs_list = (
-        "/dev", "/proc", "/sys",
-        "/etc/resolv.conf", "/etc/host.conf",
+        "/dev", "/proc", "/sys", "/etc/resolv.conf", "/etc/host.conf",
         "/lib/modules",
     )
 
@@ -263,6 +263,27 @@ class Config(object):
     docker_registries = {"docker.io": ["https://registry-1.docker.io",
                                        "https://index.docker.io"],
                         }
+
+    # nvidia files
+    nvi_etc_list = ['vulkan/icd.d/nvidia_icd.json',
+                    'OpenCL/vendors/nvidia.icd'
+                   ]
+
+    nvi_bin_list = ['nvidia-bug-report.sh', 'nvidia-cuda-mps-control',
+                    'nvidia-cuda-mps-server', 'nvidia-debugdump',
+                    'nvidia-installer', 'nvidia-persistenced',
+                    'nvidia-settings', 'nvidia-smi',
+                    'nvidia-uninstall', 'nvidia-xconfig'
+                   ]
+
+    nvi_lib_list = ['libOpenCL.', 'libcuda.', 'libnvcuvid.',
+                    'libnvidia-cfg.', 'libnvidia-compiler.',
+                    'libnvidia-encode.', 'libnvidia-fatbinaryloader.',
+                    'libnvidia-fbc.', 'libnvidia-ifr.', 'libnvidia-ml.',
+                    'libnvidia-opencl.', 'libnvidia-ptxjitcompiler.',
+                    'libnvidia-tls.', 'tls/libnvidia-tls.'
+                   ]
+
     # -------------------------------------------------------------
 
     def _verify_config(self):
@@ -1303,7 +1324,7 @@ class UdockerTools(object):
                 if "://" in tarball:
                     tarball_file = self._download(tarball)
                 elif os.path.exists(tarball):
-                    tarball_file = tarball
+                    tarball_file = os.path.realpath(tarball)
                 if tarball_file and os.path.isfile(tarball_file):
                     status = False
                     if not self._verify_version(tarball_file):
@@ -3294,7 +3315,9 @@ class FakechrootEngine(ExecutionEngineCommon):
         cmd_l.extend(["env", "-i", ])
         cmd_l.extend(self.opt["env"])
         if xmode in ("F1", "F2"):
-            cmd_l.append(self._elfpatcher.get_container_loader())
+            container_loader = self._elfpatcher.get_container_loader()
+            if container_loader:
+                cmd_l.append(container_loader)
         cmd_l.extend(self._run_add_script_support(exec_path))
         cmd_l.extend(self.opt["cmd"])
         Msg().err("CMD =", cmd_l, l=Msg.VER)
@@ -3304,6 +3327,107 @@ class FakechrootEngine(ExecutionEngineCommon):
         cwd = self._cont2host(self.opt["cwd"])
         status = subprocess.call(cmd_l, shell=False, close_fds=True, cwd=cwd)
         return status
+
+
+class NvidiaMode(object):
+    """nvidia-docker like functionality for udocker.
+    Make nvidia host libraries available within udocker, this is achieved
+    by copy them into the container so that the execution modes that need
+    to change libraries may work properly.
+    """
+
+    def __init__(self, localrepo, container_id):
+        self.localrepo = localrepo               # LocalRepository object
+        self.container_id = container_id         # Container id
+        self.container_dir = self.localrepo.cd_container(container_id)
+        self.container_root = self.container_dir + "/ROOT"
+
+    def _copy_files(self, host_src_dir, cont_dst_dir, files_list, force=False):
+        """copy or link file to destination creating directories as needed"""
+        for fname in files_list:
+            srcname = host_src_dir + '/' + fname
+            dstname = self.container_root + '/' + cont_dst_dir + '/' + fname
+            if os.path.isfile(dstname) or os.path.islink(dstname):
+                if force:
+                    try:
+                        os.remove(dstname)
+                    except OSError:
+                        Msg().err("Error: deleting nvidia file", dstname)
+                else:
+                    Msg().err("Error: nvidia file already exists", dstname,
+                              ", use --force to overwrite")
+                    return
+            if os.path.isfile(srcname):
+                srcdir = os.path.dirname(srcname)
+                dstdir = os.path.dirname(dstname)
+                if not os.path.isdir(dstdir):
+                    try:
+                        os.makedirs(dstdir)
+                        os.chmod(dstdir, stat.S_IMODE(os.stat(srcdir).st_mode) |
+                                 stat.S_IRWXU)
+                    except OSError:
+                        Msg().err("Error: creating nvidia dir", dstdir)
+                if not FileUtil(srcname).copyto(dstname):
+                    Msg().err("Error: copying file", srcname, "to", dstname)
+                    return
+                try:
+                    mask = stat.S_IMODE(os.stat(srcname).st_mode) | \
+                                        stat.S_IWUSR | stat.S_IRUSR
+                    if os.access(srcname, os.X_OK):
+                        mask = mask | stat.S_IXUSR
+                    os.chmod(dstname, mask)
+                except (IOError, OSError) as error:
+                    Msg().err("Error: change mask of nvidia file", error)
+                Msg().err("nvidia copied", srcname, "to", dstname, l=Msg.DBG)
+
+    def _get_nvidia_libs(self, host_dir):
+        """Expand the library files to include the versions"""
+        lib_list = []
+        for lib in Config.nvi_lib_list:
+            for expanded_libs in glob.glob(host_dir + '/' + lib + '*'):
+                lib_list.append(expanded_libs.replace(host_dir, ''))
+        return lib_list
+
+    def _find_host_dir(self):
+        """Find the location of the host nvidia libraries"""
+        ld_library_path = os.getenv("LD_LIBRARY_PATH")
+        if ld_library_path:
+            for libdir in ld_library_path.split(':'):
+                if glob.glob(libdir + '/libnvidia-cfg.so*'):
+                    return libdir + '/'
+        ld_data = Uprocess().get_output("ldconfig -p")
+        if not ld_data:
+            return ""
+        for line in ld_data.split("\n"):
+            match = re.search("[ |\t]libnvidia-cfg.so[^ ]* .*=> (/.*)", line)
+            if match:
+                return os.path.dirname(match.group(1)) + '/'
+        return ""
+
+    def _find_cont_dir(self):
+        """Find the location of the host target directory for libraries"""
+        for dst_dir in ('/usr/lib/x86_64-linux-gnu', '/usr/lib64'):
+            if os.path.isdir(self.container_root + "/" + dst_dir):
+                return dst_dir
+        return ""
+
+    def set_mode(self, force=False):
+        """Set nvidia mode"""
+        if not self.container_dir:
+            Msg().err("Error: nvidia set mode container dir not found")
+            return
+        nvi_host_dir = self._find_host_dir()
+        nvi_cont_dir = self._find_cont_dir()
+        if not nvi_host_dir:
+            Msg().err("Error: host nvidia libraries not found")
+            return
+        if not nvi_cont_dir:
+            Msg().err("Error: destination directory for nvidia libs not found")
+            return
+        lib_list = self._get_nvidia_libs(nvi_host_dir)
+        self._copy_files(nvi_host_dir, nvi_cont_dir, lib_list, force)
+        self._copy_files('/etc', '/etc', Config.nvi_etc_list, force)
+        self._copy_files('/usr/bin', '/usr/bin', Config.nvi_bin_list, force)
 
 
 class ExecutionMode(object):
@@ -3479,7 +3603,8 @@ class ContainerStructure(object):
         if not status:
             Msg().err("Error: creating container:", self.container_id)
         elif not self._chk_container_root():
-            Msg().err("Warning: check container content:", self.container_id)
+            Msg().err("Warning: check container content:", self.container_id,
+                      l=Msg.WAR)
         return self.container_id
 
     def create_fromlayer(self, imagerepo, tag, layer_file, container_json):
@@ -3503,7 +3628,8 @@ class ContainerStructure(object):
         if not status:
             Msg().err("Error: creating container:", self.container_id)
         elif not self._chk_container_root():
-            Msg().err("Warning: check container content:", self.container_id)
+            Msg().err("Warning: check container content:", self.container_id,
+                      l=Msg.WAR)
         return self.container_id
 
     def clone_fromfile(self, clone_file):
@@ -3521,7 +3647,8 @@ class ContainerStructure(object):
         if not status:
             Msg().err("Error: creating container clone:", self.container_id)
         elif not self._chk_container_root():
-            Msg().err("Warning: check container content:", self.container_id)
+            Msg().err("Warning: check container content:", self.container_id,
+                      l=Msg.WAR)
         return self.container_id
 
     def _apply_whiteouts(self, tarf, destdir):
@@ -3689,7 +3816,8 @@ class ContainerStructure(object):
         if not status:
             Msg().err("Error: creating container:", dest_container_id)
         elif not self._chk_container_root(dest_container_id):
-            Msg().err("Warning: check container content:", dest_container_id)
+            Msg().err("Warning: check container content:", dest_container_id,
+                      l=Msg.WAR)
         return dest_container_id
 
 
@@ -6351,6 +6479,8 @@ class Udocker(object):
         setup [options] <container-id>
         --execmode=<mode>          :select execution mode from below
         --force                    :force setup change
+        --nvidia                   :add NVIDIA libraries and binaries
+                                    (nvidia support is EXPERIMENTAL)
 
         <mode> is one of the following execution modes:
         P1: proot accelerated mode using seccomp filtering (default)
@@ -6368,6 +6498,7 @@ class Udocker(object):
         container_id = cmdp.get("P1")
         xmode = cmdp.get("--execmode=")
         force = cmdp.get("--force")
+        nvidia = cmdp.get("--nvidia")
         if cmdp.missing_options():               # syntax error
             return False
         if not self.localrepo.cd_container(container_id):
@@ -6376,6 +6507,9 @@ class Udocker(object):
         elif xmode and self.localrepo.isprotected_container(container_id):
             Msg().err("Error: container is protected")
             return False
+        if nvidia:
+            nvidia_mode = NvidiaMode(self.localrepo, container_id)
+            nvidia_mode.set_mode(force)
         exec_mode = ExecutionMode(self.localrepo, container_id)
         if xmode:
             return exec_mode.set_mode(xmode.upper(), force)
