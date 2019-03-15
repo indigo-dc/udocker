d5c48902dda17f51634ed988e0576a7a102c8653
mariojmdavid <mariojmdavid@gmail.com>
2019-03-15 15:57:46 +0000

    update MANIFEST and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""The setup script."""
+"""
+=============
+udocker setup
+=============
+Wrapper to execute basic docker containers without using docker.
+This tool is a last resort for the execution of docker containers
+where docker is unavailable. It only provides a limited set of
+functionalities.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import os
 from udocker import __version__
@@ -31,6 +47,8 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
