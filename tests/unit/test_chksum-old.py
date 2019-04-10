0866f7d0547b4fa926bc85518efd45a3c65adad3
mariojmdavid <mariojmdavid@gmail.com>
2019-04-10 07:17:59 +0100

    refactor to new test_chksum

--- tests/unit/test_chksum-old.py
+++ tests/unit/test_chksum-old.py
@@ -1,15 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 """
-udocker unit tests: ChkSUM
+udocker unit tests.
+Unit tests for udocker, a wrapper to execute basic docker containers
+without using docker.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 import os
-from unittest import TestCase
-try:
-    from unittest.mock import Mock, MagicMock, patch
-except ImportError:
-    from mock import Mock, MagicMock, patch
-
+import unittest
+import mock
 try:
     from StringIO import StringIO
 except ImportError:
@@ -24,7 +31,7 @@ def set_env():
         os.environ["HOME"] = os.getcwd()
 
 
-class ChkSUMTestCase(TestCase):
+class ChkSUMTestCase(unittest.TestCase):
     """Test ChkSUM() performs checksums portably."""
 
     @classmethod
@@ -37,11 +44,10 @@ class ChkSUMTestCase(TestCase):
         pass
 
     # TODO: review tests
-    @patch('udocker.utils.chksum.ChkSUM')
-    def test_01_sha256(self, mock_chksum):
+    def test_01_sha256(self):
         """Test ChkSUM().sha256()."""
         self._init()
-        mock_call = MagicMock()
+        mock_call = mock.MagicMock()
         cksum = ChkSUM()
         #
         mock_call.return_value = True
