c59c15ca8916aebe99534328c3a2d2776a9769cc
mariojmdavid <mariojmdavid@gmail.com>
2019-04-10 22:28:58 +0100

    small changes in test_cmdparser

--- tests/unit/test_cmdparser.py
+++ tests/unit/test_cmdparser.py
@@ -1,37 +1,13 @@
 #!/usr/bin/env python2
 """
-udocker unit tests.
-Unit tests for udocker, a wrapper to execute basic docker containers
-without using docker.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+udocker unit tests: CmdParser
 """
-
-import os
-import unittest
+from unittest import TestCase, main
 from udocker.cmdparser import CmdParser
 
 
-def set_env():
-    """Set environment variables."""
-    if not os.getenv("HOME"):
-        os.environ["HOME"] = os.getcwd()
-
-
-class CmdParserTestCase(unittest.TestCase):
+class CmdParserTestCase(TestCase):
     """Test CmdParserTestCase() command line interface."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Setup test."""
-        set_env()
 
     def test_01__init(self):
         """Test CmdParser() Constructor."""
@@ -99,4 +75,4 @@ class CmdParserTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
