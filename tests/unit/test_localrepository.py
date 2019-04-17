053d300ff152bfbfb27b61067fe8f9717d059aea
mariojmdavid <mariojmdavid@gmail.com>
2019-04-17 22:45:15 +0100

    fix shebang to python

--- tests/unit/test_localrepository.py
+++ tests/unit/test_localrepository.py
@@ -1,28 +1,20 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 """
-udocker unit tests.
-Unit tests for udocker, a wrapper to execute basic docker containers
-without using docker.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+udocker unit tests: LocalRepository
 """
-
-import os
 import sys
+import os
 import subprocess
-import unittest
-import mock
+from unittest import TestCase, main
+try:
+    from unittest.mock import Mock, patch, MagicMock, mock_open
+except ImportError:
+    from mock import Mock, patch, MagicMock, mock_open
 
 sys.path.append('.')
 
 from udocker.container.localrepo import LocalRepository
+from udocker.config import Config
 
 UDOCKER_TOPDIR = "test_topdir"
 if sys.version_info[0] >= 3:
