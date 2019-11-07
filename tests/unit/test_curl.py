b4406a4e7478cadb39d9203026082ea31e95b105
mariojmdavid <mariojmdavid@gmail.com>
2019-11-07 22:56:33 +0000

    remove unused imports

--- tests/unit/test_curl.py
+++ tests/unit/test_curl.py
@@ -6,23 +6,20 @@ udocker unit tests: CurlHeader
 """
 import sys
 from unittest import TestCase, main
-try:
-    from unittest.mock import Mock, MagicMock, patch, mock_open
-except ImportError:
-    from mock import Mock, MagicMock, patch, mock_open
-
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-
-sys.path.append('.')
-
 from udocker.utils.curl import CurlHeader
 from udocker.utils.curl import GetURL
 from udocker.utils.curl import GetURLpyCurl
 from udocker.utils.curl import GetURLexeCurl
 from udocker.config import Config
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 if sys.version_info[0] >= 3:
     BUILTINS = "builtins"
