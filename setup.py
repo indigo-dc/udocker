ef1fe1549d446a0ef521933faae92e2d1927830f
Jorge Gomes <jorge@lip.pt>
2019-12-19 17:57:38 +0000

    markdown into pypi

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme + '\n\n' + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords='udocker',
     name='udocker',
