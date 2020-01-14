a9708a8b4fbf7be9f176cff84a7a8d858692b78b
mariojmdavid <mariojmdavid@gmail.com>
2020-01-14 17:56:08 +0000

    unit test for unique

--- tests/unit/test_unique.py
+++ tests/unit/test_unique.py
@@ -15,19 +15,19 @@ class UniqueTestCase(TestCase):
     """Test Unique() class."""
 
     def test_01_init(self):
-        """Test Unique() constructor."""
+        """Test01 Unique() constructor."""
         uniq = Unique()
         self.assertEqual(uniq.def_name, "udocker")
 
     def test_02__rnd(self):
-        """Test Unique._rnd()."""
+        """Test02 Unique._rnd()."""
         uniq = Unique()
         rand = uniq._rnd(64)
         self.assertIsInstance(rand, str)
         self.assertEqual(len(rand), 64)
 
     def test_03_uuid(self):
-        """Test Unique.uuid()."""
+        """Test03 Unique.uuid()."""
         uniq = Unique()
         rand = uniq.uuid("zxcvbnm")
         self.assertEqual(len(rand), 36)
@@ -35,19 +35,25 @@ class UniqueTestCase(TestCase):
         self.assertEqual(len(rand), 36)
 
     def test_04_imagename(self):
-        """Test Unique.imagename()."""
+        """Test04 Unique.imagename()."""
         uniq = Unique()
         rand = uniq.imagename()
         self.assertEqual(len(rand), 16)
 
-    def test_05_layer_v1(self):
-        """Test Unique.layer_v1()."""
+    def test_05_imagetag(self):
+        """Test05 Unique.imagetag()."""
+        uniq = Unique()
+        rand = uniq.imagetag()
+        self.assertEqual(len(rand), 10)
+
+    def test_06_layer_v1(self):
+        """Test06 Unique.layer_v1()."""
         uniq = Unique()
         rand = uniq.layer_v1()
         self.assertEqual(len(rand), 64)
 
-    def test_06_filename(self):
-        """Test Unique.filename()."""
+    def test_07_filename(self):
+        """Test07 Unique.filename()."""
         uniq = Unique()
         rand = uniq.filename("zxcvbnmasdf")
         self.assertTrue(rand.endswith("zxcvbnmasdf"))
