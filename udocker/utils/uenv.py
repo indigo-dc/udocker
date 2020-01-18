df6e39af46173184229e9d7631a6a49d41ca1333
Jorge Gomes <jorge@lip.pt>
2020-01-18 20:59:01 +0000

    get_pair() default

--- udocker/utils/uenv.py
+++ udocker/utils/uenv.py
@@ -6,7 +6,7 @@ import sys
 import string
 
 def get_pair(envstr):
-    """Split env var into key and val"""
+    """Split env=var into key and val"""
     if sys.version_info[0] >= 3:
         if not (isinstance(envstr, (str)) and envstr):
            return ("", "")
@@ -45,7 +45,7 @@ class UenvIterator(object):
         self._index = 0
 
     def __next__(self):
-        """Python 3 next key and value of environment"""
+        """Python 3 next (key, value) of environment"""
         if self._index < len(self._keys):
             key = self._keys[self._index]
             value = self._uenv.env[key]
@@ -57,7 +57,7 @@ class UenvIterator(object):
 
 
 class Uenv(object):
-    """Manipulate environment class"""
+    """Class to store environment variables"""
 
     def __init__(self, thisenv=None):
         """Init Uenv instance"""
@@ -66,29 +66,29 @@ class Uenv(object):
             raise ValueError('invalid environment')
 
     def __iter__(self):
-        """Returns the iterator"""
+        """Returns the Uenv iterator"""
         return UenvIterator(self)
 
     def __len__(self):
-        """Returns the lenght number of env variables"""
+        """Returns the number of stored env variables"""
         return len(self.env)
 
     def append(self, envstr):
-        """Add var to environment"""
+        """Add string with key=val to Uenv"""
         (key, val) = get_pair(envstr)
         if key:
             self.env[key] = val
         return self
 
     def appendif(self, envstr):
-        """Add var to environment if not existent"""
+        """Add string with key=val to Uenv if key non-existent"""
         (key, val) = get_pair(envstr)
         if key and key not in self.env:
             self.env[key] = val
         return self
 
     def extend(self, envlist):
-        """Extend environment add list of vars"""
+        """Extend Uenv stored environment with list or dict of key=val"""
         if isinstance(envlist, (list)):
             for envstr in envlist:
                 self.append(envstr)
@@ -98,7 +98,7 @@ class Uenv(object):
         return self
 
     def extendif(self, envlist):
-        """Extend environment add list of vars"""
+        """Extend Uenv environment with list of key=val if key non-existant"""
         if isinstance(envlist, (list)):
             for envstr in envlist:
                 self.appendif(envstr)
@@ -109,7 +109,7 @@ class Uenv(object):
         return self
 
     def add(self, thisenv):
-        """Extend environment add list of vars"""
+        """Extend Uenv environment with str or list with key=val"""
         if isinstance(thisenv, (str)):
             self.append(thisenv)
         elif isinstance(thisenv, (list)):
@@ -117,7 +117,7 @@ class Uenv(object):
         return self
 
     def getenv(self, key):
-        """Get variable name"""
+        """Get variable by name"""
         try:
             return self.env[key]
         except KeyError:
@@ -128,7 +128,7 @@ class Uenv(object):
         return self.append('%s=%s' % (key, val))
 
     def unsetenv(self, key):
-        """Delete variable name"""
+        """Delete variable by name"""
         try:
             del self.env[key]
             return True
@@ -136,7 +136,7 @@ class Uenv(object):
             return False
 
     def list(self):
-        """Get list with environment"""
+        """Get list with environment variables"""
         env_list = []
         for (key, val) in self.env.items():
             env_list.append('%s=%s' % (key, val))
@@ -147,5 +147,5 @@ class Uenv(object):
         return self.env
 
     def keys(self):
-        """Get dict with environment"""
+        """Get list of environment variable names"""
         return self.env.keys()
