318ec5ed7468dde0a9d269f59a76e38d0019e091
Jorge Gomes <jorge@lip.pt>
2021-05-04 18:59:22 +0100

    Fix oskernel_isgreater()

--- udocker/helper/hostinfo.py
+++ udocker/helper/hostinfo.py
@@ -62,7 +62,7 @@ class HostInfo(object):
         """Compare kernel version is greater or equal than ref_version
         version: list [int, int, int]
         """
-        os_release = self.oskernel().split('-')[0]
+        os_release = self.oskernel().split('-')[0].split('+')[0]
         os_version = [int(x) for x in os_release.split('.')[0:3]]
         for idx in (0, 1, 2):
             if os_version[idx] > version[idx]:
