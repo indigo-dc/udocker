a4361f20b6c4d5601d7fb96b6f8cf985841e870d
mariojmdavid <mariojmdavid@gmail.com>
2021-07-21 15:22:12 +0100

    implement logging in runc

--- udocker/engine/runc.py
+++ udocker/engine/runc.py
@@ -8,10 +8,11 @@ import platform
 import stat
 import select
 import json
+import logging
 
+from udocker import LOG
 from udocker.engine.base import ExecutionEngineCommon
 from udocker.engine.proot import PRootEngine
-from udocker.msg import Msg
 from udocker.config import Config
 from udocker.utils.fileutil import FileUtil
 from udocker.utils.uvolume import Uvolume
@@ -68,8 +69,9 @@ class RuncEngine(ExecutionEngineCommon):
             self.executable = f_util.find_file_in_dir(image_list)
 
         if not os.path.exists(self.executable):
-            Msg().err("Error: runc/crun executable not found")
+            LOG.error("runc/crun executable not found")
             sys.exit(1)
+
         if "crun" in os.path.basename(self.executable):
             self.engine_type = "crun"
         elif "runc" in os.path.basename(self.executable):
@@ -83,7 +85,7 @@ class RuncEngine(ExecutionEngineCommon):
 
         if FileUtil(self._container_specfile).size() == -1:
             cmd_l = [self.executable, "spec", "--rootless", ]
-            status = subprocess.call(cmd_l, shell=False, stderr=Msg.chlderr,
+            status = subprocess.call(cmd_l, shell=False, stderr=sys.stderr,
                                      close_fds=True,
                                      cwd=os.path.realpath(\
                                          self._container_specdir))
@@ -114,6 +116,7 @@ class RuncEngine(ExecutionEngineCommon):
             if outfile:
                 outfile.close()
             return False
+
         outfile.close()
         return True
 
@@ -151,6 +154,7 @@ class RuncEngine(ExecutionEngineCommon):
         else:
             json_obj["linux"]["uidMappings"] = [ \
                     {"containerID": 0, "hostID": HostInfo.uid, "size":1, }, ]
+
         if "gidMappings" in json_obj["linux"]:
             for idmap in json_obj["linux"]["gidMappings"]:
                 if "hostID" in idmap:
@@ -171,13 +175,13 @@ class RuncEngine(ExecutionEngineCommon):
         """Check the uid_map string for container run command"""
         if ("user" in self.opt and self.opt["user"] != "0" and
                 self.opt["user"] != "root"):
-            Msg().out("Warning: this engine only supports execution as root",
-                      l=Msg.WAR)
+            LOG.warning("this engine only supports execution as root")
 
     def _add_capabilities_spec(self):
         """Set the spec capabilities"""
         if not Config.conf['runc_capabilities']:
             return
+
         self._container_specjson["process"]["capabilities"]["ambient"] = \
             Config.conf['runc_capabilities']
         self._container_specjson["process"]["capabilities"]["bounding"] = \
@@ -192,25 +196,31 @@ class RuncEngine(ExecutionEngineCommon):
     def _add_device_spec(self, dev_path, mode="rwm"):
         """Add device to the configuration"""
         if not (os.path.exists(dev_path) and dev_path.startswith("/dev/")):
-            Msg().err("Error: device not found", dev_path)
+            LOG.error("device not found: %s", dev_path)
             return False
+
         dev_stat = os.stat(dev_path)
         if stat.S_ISBLK(dev_stat.st_mode):
             dev_type = 'b'
         elif stat.S_ISCHR(dev_stat.st_mode):
             dev_type = 'c'
         else:
-            Msg().err("Error: not a device", dev_path)
+            LOG.error("not a device: %s", dev_path)
             return False
+
         filemode = 0
         if 'r' in mode.lower():
             filemode += 0o444
+
         if 'w' in mode.lower():
             filemode += 0o222
+
         if not filemode:
             filemode = 0o666
+
         if "devices" not in self._container_specjson["linux"]:
             self._container_specjson["linux"]["devices"] = []
+
         device = {
             "path": dev_path,
             "type": dev_type,
@@ -230,6 +240,7 @@ class RuncEngine(ExecutionEngineCommon):
             dev_name = dev.split(':')[0]
             if self._add_device_spec(dev_name):
                 added_devices.append(dev_name)
+
         if "/dev/nvidiactl" not in added_devices:
             nvidia_mode = NvidiaMode(self.localrepo, self.container_id)
             if nvidia_mode.get_mode():
@@ -244,12 +255,15 @@ class RuncEngine(ExecutionEngineCommon):
             mode = "rw"
         else:
             mode = "ro"
+
         mount = {"destination": cont_dest,
                  "type": fstype,
                  "source": host_source,
                  "options": ["rbind", "nosuid", "nodev", mode, ], }
+
         if options is not None:
             mount["options"] = options
+
         self._container_specjson["mounts"].append(mount)
 
     def _del_mount_spec(self, host_source, cont_dest):
@@ -264,6 +278,7 @@ class RuncEngine(ExecutionEngineCommon):
             if (mount["destination"] == cont_dest and
                     mount["source"] == host_source):
                 return index
+
         return None
 
     def _mod_mount_spec(self, host_source, cont_dest, new):
@@ -271,6 +286,7 @@ class RuncEngine(ExecutionEngineCommon):
         index = self._sel_mount_spec(host_source, cont_dest)
         if index is None:
             return False
+
         mount = self._container_specjson["mounts"][index]
         for new_item in new:
             if new_item == "options":
@@ -281,9 +297,11 @@ class RuncEngine(ExecutionEngineCommon):
                     for (index, opt) in enumerate(mount["options"]):
                         if opt.startswith(option_prefix):
                             del mount["options"][index]
+
                     mount["options"] += new["options"]
             else:
                 mount[new_item] = new[new_item]
+
         return True
 
     def _add_volume_bindings(self):
@@ -294,16 +312,16 @@ class RuncEngine(ExecutionEngineCommon):
             (host_dir, cont_dir) = Uvolume(vol).split()
             if os.path.isdir(host_dir):
                 if host_dir == "/dev":
-                    Msg().out("Warning: engine does not support -v",
-                              host_dir, l=Msg.WAR)
+                    LOG.warning"engine does not support -v %s", host_dir)
                     continue
+
                 self._add_mount_spec(host_dir, cont_dir, rwmode=True)
             elif os.path.isfile(host_dir):
                 if (host_dir not in Config.conf['sysdirs_list'] and
                         host_dir + ":" + cont_dir not in self.hostauth_list and
                         os.path.dirname(cont_dir) not in
                         Config.conf['mountpoint_prefixes']):
-                    Msg().err("Error: engine does not support file mounting:",
+                    LOG.error("engine does not support file mounting: %s",
                               host_dir)
                 else:
                     self._filebind.set_file(host_dir, cont_dir)
@@ -312,11 +330,11 @@ class RuncEngine(ExecutionEngineCommon):
     def _run_invalid_options(self):
         """check -p --publish -P --publish-all --net-coop"""
         if self.opt["portsmap"]:
-            Msg().out("Warning: this execution mode does not support "
-                      "-p --publish", l=Msg.WAR)
+            LOG.warning("this execution mode does not support -p --publish")
+
         if self.opt["netcoop"]:
-            Msg().out("Warning: this execution mode does not support "
-                      "-P --netcoop --publish-all", l=Msg.WAR)
+            LOG.warning("this exec mode does not support -P "
+                        "--netcoop --publish-all")
 
     def _proot_overlay(self, proot_mode="P2"):
         """Execute proot within runc"""
@@ -357,18 +375,15 @@ class RuncEngine(ExecutionEngineCommon):
           * argument: container_id or name
           * options:  many via self.opt see the help
         """
-
         Config.conf['sysdirs_list'] = (
             "/etc/resolv.conf", "/etc/host.conf",
             # "/etc/passwd", "/etc/group",
         )
-
         # setup execution
         if not self._run_init(container_id):
             return 2
 
         self._run_invalid_options()
-
         self._container_specfile = "config.json"
         if self.container_dir:
             if self.localrepo.iswriteable_container(container_id):
@@ -376,24 +391,20 @@ class RuncEngine(ExecutionEngineCommon):
             else:
                 self._container_specdir = FileUtil("SPECDIR").mktmpdir()
                 FileUtil(self._container_specdir).register_prefix()
+
             self._container_specfile = \
                     self._container_specdir + '/' + self._container_specfile
 
         self._filebind = FileBind(self.localrepo, container_id)
         self._filebind.setup()
         self.select_runc()
-
         # create new OCI spec file
         if not self._load_spec(new=True):
             return 4
 
         self._uid_check()
-
-        # if not --hostenv clean the environment
-        self._run_env_cleanup_list()
-
-        # set environment variables
-        self._run_env_set()
+        self._run_env_cleanup_list()  # if not --hostenv clean the environment
+        self._run_env_set()    # set environment variables
         self._set_spec()
         if (Config.conf['runc_nomqueue'] or
                 (Config.conf['runc_nomqueue'] is None and not
@@ -409,9 +420,9 @@ class RuncEngine(ExecutionEngineCommon):
         self._mod_mount_spec("shm", "/dev/shm", {"options": ["size=2g"]})
         self._proot_overlay()
         self._save_spec()
-        if Msg.level >= Msg.DBG:
+        if Config.conf['verbose_level'] == logging.DEBUG:
             runc_debug = ["--debug", ]
-            Msg().out(json.dumps(self._container_specjson,
+            LOG.debug(json.dumps(self._container_specjson,
                                  indent=4, sort_keys=True))
         else:
             runc_debug = []
@@ -423,7 +434,7 @@ class RuncEngine(ExecutionEngineCommon):
         cmd_l.extend(runc_debug)
         cmd_l.extend(["--root", self._container_specdir, "run"])
         cmd_l.extend(["--bundle", self._container_specdir, self.execution_id])
-        Msg().err("CMD =", cmd_l, l=Msg.VER)
+        LOG.info("CMD = %s", cmd_l)
         self._run_banner(self.opt["cmd"][0], '%')
         if sys.stdout.isatty():
             return self.run_pty(cmd_l)
@@ -446,10 +457,13 @@ class RuncEngine(ExecutionEngineCommon):
             status.poll()
             if status.returncode is not None:
                 break
+
             readable, dummy, exception = \
                 select.select([pmaster, ], [], [pmaster, ], 5)
+
             if exception:
                 break
+
             if readable:
                 try:
                     if sys.version_info[0] >= 3:
@@ -458,9 +472,11 @@ class RuncEngine(ExecutionEngineCommon):
                         sys.stdout.write(os.read(pmaster, 1))
                 except OSError:
                     break
+
         try:
             status.terminate()
         except OSError:
             pass
+
         self._filebind.finish()
         return status
