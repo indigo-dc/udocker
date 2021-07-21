36837154ae520b1025a96ef2a919a3a9c2dc84e0
mariojmdavid <mariojmdavid@gmail.com>
2021-07-21 13:20:28 +0100

    implement logging structure

--- udocker/container/structure.py
+++ udocker/container/structure.py
@@ -2,11 +2,12 @@
 """Tools to manage the container structure"""
 
 import os
+import sys
 import subprocess
+import logging
 
-from udocker import is_genstr
+from udocker import is_genstr, LOG
 from udocker.config import Config
-from udocker.msg import Msg
 from udocker.utils.fileutil import FileUtil
 from udocker.utils.uprocess import Uprocess
 from udocker.helper.unique import Unique
@@ -34,13 +35,13 @@ class ContainerStructure(object):
         else:
             container_dir = self.localrepo.cd_container(self.container_id)
             if not container_dir:
-                Msg().err("Error: container id or name not found")
+                LOG.error("container id or name not found")
                 return(False, False)
 
             fjson = container_dir + "/container.json"
             container_json = self.localrepo.load_json(fjson)
             if not container_json:
-                Msg().err("Error: invalid container json metadata")
+                LOG.error("invalid container json metadata")
                 return(False, False)
 
         return(container_dir, container_json)
@@ -52,6 +53,7 @@ class ContainerStructure(object):
             confidx = "config"
         elif "container_config" in container_json:
             confidx = "container_config"
+
         if container_json[confidx]  and param in container_json[confidx]:
             if container_json[confidx][param] is None:
                 pass
@@ -69,14 +71,15 @@ class ContainerStructure(object):
                 return self._dict_to_list(container_json[confidx][param])
             else:
                 return container_json[confidx][param]
+
         return default
 
-    # DEBUG
     def _dict_to_str(self, in_dict):
         """Convert dict to str"""
         out_str = ""
         for (key, val) in in_dict.items():
             out_str += "%s:%s " % (str(key), str(val))
+
         return out_str
 
     def _dict_to_list(self, in_dict):
@@ -84,6 +87,7 @@ class ContainerStructure(object):
         out_list = []
         for (key, val) in in_dict.items():
             out_list.append("%s:%s" % (str(key), str(val)))
+
         return out_list
 
     def _chk_container_root(self, container_id=None):
@@ -92,8 +96,10 @@ class ContainerStructure(object):
             container_dir = self.localrepo.cd_container(container_id)
         else:
             container_dir = self.localrepo.cd_container(self.container_id)
+
         if not container_dir:
             return 0
+
         container_root = container_dir + "/ROOT"
         check_list = ["/lib", "/bin", "/etc", "/tmp", "/var", "/usr", "/sys",
                       "/dev", "/data", "/home", "/system", "/root", "/proc", ]
@@ -101,6 +107,7 @@ class ContainerStructure(object):
         for f_path in check_list:
             if os.path.exists(container_root + f_path):
                 found += 1
+
         return found
 
     def create_fromimage(self, imagerepo, tag):
@@ -115,12 +122,12 @@ class ContainerStructure(object):
         self.tag = tag
         image_dir = self.localrepo.cd_imagerepo(self.imagerepo, self.tag)
         if not image_dir:
-            Msg().err("Error: create container: imagerepo is invalid")
+            LOG.error("create container: imagerepo is invalid")
             return False
 
         (container_json, layer_files) = self.localrepo.get_image_attributes()
         if not container_json:
-            Msg().err("Error: create container: getting layers or json")
+            LOG.error("create container: getting layers or json")
             return False
 
         if not self.container_id:
@@ -129,17 +136,16 @@ class ContainerStructure(object):
         container_dir = self.localrepo.setup_container(
             self.imagerepo, self.tag, self.container_id)
         if not container_dir:
-            Msg().err("Error: create container: setting up container")
+            LOG.error("create container: setting up container")
             return False
 
         self.localrepo.save_json(
             container_dir + "/container.json", container_json)
         status = self._untar_layers(layer_files, container_dir + "/ROOT")
         if not status:
-            Msg().err("Error: creating container:", self.container_id)
+            LOG.error("creating container: %s", self.container_id)
         elif not self._chk_container_root():
-            Msg().out("Warning: check container content:", self.container_id,
-                      l=Msg.WAR)
+            LOG.warning("check container content: %s", self.container_id)
 
         return self.container_id
 
@@ -152,23 +158,22 @@ class ContainerStructure(object):
             self.container_id = Unique().uuid(os.path.basename(self.imagerepo))
 
         if not container_json:
-            Msg().err("Error: create container: getting json")
+            LOG.error("create container: getting json")
             return False
 
         container_dir = self.localrepo.setup_container(
             self.imagerepo, self.tag, self.container_id)
         if not container_dir:
-            Msg().err("Error: create container: setting up")
+            LOG.error("create container: setting up")
             return False
 
         fjson = container_dir + "/container.json"
         self.localrepo.save_json(fjson, container_json)
         status = self._untar_layers([layer_file, ], container_dir + "/ROOT")
         if not status:
-            Msg().err("Error: creating container:", self.container_id)
+            LOG.error("creating container: %s", self.container_id)
         elif not self._chk_container_root():
-            Msg().out("Warning: check container content:", self.container_id,
-                      l=Msg.WAR)
+            LOG.warning("check container content: %s", self.container_id)
 
         return self.container_id
 
@@ -182,15 +187,14 @@ class ContainerStructure(object):
         container_dir = self.localrepo.setup_container(
             "CLONING", "inprogress", self.container_id)
         if not container_dir:
-            Msg().err("Error: create container: setting up")
+            LOG.error("create container: setting up")
             return False
 
         status = self._untar_layers([clone_file, ], container_dir)
         if not status:
-            Msg().err("Error: creating container clone:", self.container_id)
+            LOG.error("creating container clone: %s", self.container_id)
         elif not self._chk_container_root():
-            Msg().out("Warning: check container content:", self.container_id,
-                      l=Msg.WAR)
+            LOG.warning("check container content:", self.container_id)
 
         return self.container_id
 
@@ -200,16 +204,19 @@ class ContainerStructure(object):
         The format is .wh.<filename>
         """
         verbose = ""
-        if Msg.level >= Msg.VER:
+        if Config.conf['verbose_level'] == logging.DEBUG:
             verbose = 'v'
-            Msg().out("Info: applying whiteouts:", tarf, l=Msg.INF)
+            LOG.debug("applying whiteouts: %s", tarf)
+
         wildcards = ["--wildcards", ]
         if not HostInfo().cmd_has_option("tar", wildcards[0]):
             wildcards = []
+
         cmd = ["tar", "t" + verbose] + wildcards + ["-f", tarf, r"*/.wh.*"]
         whiteouts = Uprocess().get_output(cmd, True)
         if not whiteouts:
             return
+
         for wh_filename in whiteouts.split('\n'):
             if wh_filename:
                 wh_basename = os.path.basename(wh_filename.strip())
@@ -217,6 +224,7 @@ class ContainerStructure(object):
                 if wh_basename == ".wh..wh..opq":
                     if not os.path.isdir(destdir + '/' + wh_dirname):
                         continue
+
                     for f_name in os.listdir(destdir + '/' + wh_dirname):
                         rm_filename = destdir + '/' \
                             + wh_dirname + '/' + f_name
@@ -226,6 +234,7 @@ class ContainerStructure(object):
                         + wh_dirname + '/' \
                         + wh_basename.replace(".wh.", "", 1)
                     FileUtil(rm_filename).remove(recursive=True)
+
         return
 
     def _untar_layers(self, tarfiles, destdir):
@@ -235,27 +244,32 @@ class ContainerStructure(object):
         """
         if not (tarfiles and destdir):
             return False
+
         status = True
         gid = str(HostInfo.gid)
         optional_flags = ["--wildcards", "--delay-directory-restore", ]
         for option in optional_flags:
             if not HostInfo().cmd_has_option("tar", option):
                 optional_flags.remove(option)
+
         for tarf in tarfiles:
             if tarf != '-':
                 self._apply_whiteouts(tarf, destdir)
+
             verbose = ''
-            if Msg.level >= Msg.VER:
+            if Config.conf['verbose_level'] == logging.DEBUG:
                 verbose = 'v'
-                Msg().out("Info: extracting:", tarf, l=Msg.INF)
+                LOG.debug("extracting: %s", tarf)
+
             cmd = ["tar", "-C", destdir, "-x" + verbose,
                    "--one-file-system", "--no-same-owner", "--overwrite",
                    "--exclude=dev/*", "--exclude=etc/udev/devices/*",
                    "--no-same-permissions", r"--exclude=.wh.*",
                    ] + optional_flags + ["-f", tarf]
-            if subprocess.call(cmd, stderr=Msg.chlderr, close_fds=True):
-                Msg().err("Error: while extracting image layer")
+            if subprocess.call(cmd, stderr=sys.stderr, close_fds=True):
+                LOG.error("while extracting image layer")
                 status = False
+
             cmd = ["find", destdir,
                    "(", "-type", "d", "!", "-perm", "-u=x", "-exec",
                    "chmod", "u+x", "{}", ";", ")", ",",
@@ -267,9 +281,11 @@ class ContainerStructure(object):
                    "{}", ";", ")", ",",
                    "(", "-name", ".wh.*", "-exec", "rm", "-f",
                    "--preserve-root", "{}", ";", ")"]
-            if subprocess.call(cmd, stderr=Msg.chlderr, close_fds=True):
+
+            if subprocess.call(cmd, stderr=sys.stderr, close_fds=True):
                 status = False
-                Msg().err("Error: while modifying attributes of image layer")
+                LOG.error("while modifying attributes of image layer")
+
         return status
 
     def export_tofile(self, clone_file):
@@ -277,13 +293,12 @@ class ContainerStructure(object):
         """
         cont_dir = self.localrepo.cd_container(self.container_id)
         if not cont_dir:
-            Msg().err("Error: container not found:", self.container_id)
+            LOG.error("container not found: %s", self.container_id)
             return False
 
         status = FileUtil(cont_dir + "/ROOT").tar(clone_file)
         if not status:
-            Msg().err("Error: exporting container file system:",
-                      self.container_id)
+            LOG.error("exporting container file system: %s", self.container_id)
 
         return self.container_id
 
@@ -293,12 +308,12 @@ class ContainerStructure(object):
         """
         container_dir = self.localrepo.cd_container(self.container_id)
         if not container_dir:
-            Msg().err("Error: container not found:", self.container_id)
+            LOG.error("container not found: %s", self.container_id)
             return False
 
         status = FileUtil(container_dir).tar(clone_file)
         if not status:
-            Msg().err("Error: export container as clone:", self.container_id)
+            LOG.error("export container as clone: %s", self.container_id)
 
         return self.container_id
 
@@ -307,23 +322,22 @@ class ContainerStructure(object):
         """
         source_container_dir = self.localrepo.cd_container(self.container_id)
         if not source_container_dir:
-            Msg().err("Error: source container not found:", self.container_id)
+            LOG.error("source container not found: %s", self.container_id)
             return False
 
         dest_container_id = Unique().uuid(os.path.basename(self.imagerepo))
         dest_container_dir = self.localrepo.setup_container(
             "CLONING", "inprogress", dest_container_id)
         if not dest_container_dir:
-            Msg().err("Error: create destination container: setting up")
+            LOG.error("create destination container: setting up")
             return False
 
         status = FileUtil(source_container_dir).copydir(dest_container_dir)
         if not status:
-            Msg().err("Error: creating container:", dest_container_id)
+            LOG.error("creating container: %s", dest_container_id)
             return False
 
         if not self._chk_container_root(dest_container_id):
-            Msg().out("Warning: check container content:", dest_container_id,
-                      l=Msg.WAR)
+            LOG.warning("check container content: %s", dest_container_id)
 
         return dest_container_id
