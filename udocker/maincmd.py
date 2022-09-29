b46f5939cfbcca53f9879c67aff22384c7a41051
Jorge Gomes <jorge@lip.pt>
2022-09-29 14:22:58 +0100

    replace abspath by realpath

--- udocker/maincmd.py
+++ udocker/maincmd.py
@@ -21,7 +21,7 @@ limitations under the License.
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(sys.argv[0])) + '/../')
+sys.path.append(os.path.dirname(os.path.realpath(sys.argv[0])) + '/../')
 
 # pylint: disable=wrong-import-position
 from msg import Msg
