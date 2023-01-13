f22e4c2c3eb7ee9ea112c423f5822d3de57fe1c4
mariojmdavid <mariojmdavid@gmail.com>
2023-01-13 10:27:29 +0000

    WIP on reintroduce udocker namespace - __init__.py

--- udocker/__init__.py
+++ udocker/__init__.py
@@ -21,8 +21,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import sys
 __author__ = "udocker@lip.pt"
-__copyright__ = "Copyright 2016 - 2022, LIP"
+__copyright__ = "Copyright 2016 - 2023, LIP"
 __credits__ = [
     "PRoot http://proot.me",
     "runC https://runc.io",
@@ -31,5 +32,18 @@ __credits__ = [
     "Singularity http://singularity.lbl.gov"
     ]
 __license__ = "Licensed under the Apache License, Version 2.0"
-__version__ = "1.3.5"
-__date__ = "2022"
+__version__ = "1.3.6"
+__date__ = "2023"
+
+def is_genstr(objstr):
+    """test if objstr is string or unicode both in py2 nd py3
+    unicode type has been removed in py3
+    :param objstr: object to test if string or unicode
+    :return (bool) is_gstr if it is string or unicode or not
+    """
+    is_gstr = False
+    if sys.version_info[0] >= 3:
+        is_gstr = isinstance(objstr, str)
+    else:
+        is_gstr = isinstance(objstr, (str, unicode))
+    return is_gstr
