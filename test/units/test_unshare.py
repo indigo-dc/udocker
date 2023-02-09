fc87bf7a2850cd16cd758c51673e98d15b75f4e5
mariojmdavid <mariojmdavid@gmail.com>
2023-02-09 15:44:25 +0000

    complete test_unshare

--- test/units/test_unshare.py
+++ test/units/test_unshare.py
@@ -2,6 +2,7 @@
 """
 udocker unit tests: Unshare
 """
+from unittest.mock import MagicMock
 from udocker.helper.unshare import Unshare
 
 
@@ -25,68 +26,118 @@ def test_02_unshare(mocker):
 def test_03_unshare(mocker):
     """Test03 Unshare().unshare"""
     mock_cdll = mocker.patch('ctypes.CDLL', side_effect=OSError)
+    mock_logerr = mocker.patch('udocker.LOG.error')
     status = Unshare().unshare(False)
     mock_cdll.assert_called()
+    mock_logerr.assert_called()
     assert not status
 
 
-    # mock_cdll.return_value.unshare.return_value = -1
-    # status = Unshare().unshare(True)
-    # self.assertTrue(mock_cdll.return_value.unshare.called)
-    # self.assertFalse(status)
+def test_04_namespace_exec(mocker):
+    """Test04 Unshare().namespace_exec cpid = 0"""
+    mock_method = MagicMock(name='method')
+    mock_pipe = mocker.patch('os.pipe', side_effect=[('pread1', 'pwrite1'),('pread1', 'pwrite1')])
+    mock_fork = mocker.patch('os.fork', return_value=0)
+    mock_unshare = mocker.patch.object(Unshare, 'unshare', return_value=True)
+    mock_close = mocker.patch('os.close', side_effect=[None, None])
+    mock_read = mocker.patch('os.read')
+    mock_setgid = mocker.patch('os.setgid')
+    moc_setuid = mocker.patch('os.setuid')
+    mock_setgrp = mocker.patch('os.setgroups')
+    mock_sysexit = mocker.patch('sys.exit', return_value=1)
 
-# @patch('udocker.helper.unshare.os._exit')
-# @patch.object(Unshare, 'unshare')
-# @patch('udocker.helper.unshare.os.setgroups')
-# @patch('udocker.helper.unshare.os.setuid')
-# @patch('udocker.helper.unshare.os.setgid')
-# @patch('udocker.helper.unshare.os.waitpid')
-# @patch('udocker.helper.unshare.NixAuthentication.user_in_subgid')
-# @patch('udocker.helper.unshare.subprocess.call')
-# @patch('udocker.helper.unshare.NixAuthentication.user_in_subuid')
-# @patch('udocker.helper.unshare.HostInfo')
-# @patch('udocker.helper.unshare.os.read')
-# @patch('udocker.helper.unshare.os.close')
-# @patch('udocker.helper.unshare.os.fork')
-# @patch('udocker.helper.unshare.os.pipe')
-# def test_02_namespace_exec(self, mock_pipe, mock_fork, mock_close,
-#                             mock_read, mock_hinfo, mock_usubuid,
-#                             mock_call, mock_usubgid, mock_wait,
-#                             mock_setgid, moc_setuid, mock_setgrp,
-#                             mock_ush, mock_exit):
-#     """Test02 Unshare().namespace_exec"""
+    status = Unshare().namespace_exec(mock_method)
+    assert status is None
+    assert mock_pipe.call_count == 2
+    mock_fork.assert_called()
+    mock_unshare.assert_called()
+    assert mock_close.call_count == 2
+    mock_read.assert_called()
+    mock_setgid.assert_called()
+    moc_setuid.assert_called()
+    mock_setgrp.assert_called()
+    mock_sysexit.assert_called()
 
-#     # cpid exists waitpid=0
-#     mock_pipe.side_effect = [('rfid1', 'wfid1'), ('rfid2', 'wfid2')]
-#     mock_fork.return_value = 1234
-#     mock_close.side_effect = [None, None]
-#     mock_read.return_value = None
-#     mock_hinfo.return_value.username.return_value = 'user'
-#     mock_hinfo.uid = 1000
-#     mock_usubuid.return_value = [(1000, 1000)]
-#     mock_usubgid.return_value = [(1000, 1000)]
-#     mock_call.side_effect = [None, None]
-#     mock_wait.return_value = (123, 0)
-#     status = Unshare().namespace_exec('1')
-#     self.assertEqual(mock_pipe.call_count, 2)
-#     self.assertTrue(mock_fork.called)
-#     self.assertEqual(mock_close.call_count, 2)
-#     self.assertTrue(mock_hinfo.return_value.username.called)
-#     self.assertTrue(mock_usubuid.called)
-#     self.assertTrue(mock_usubgid.called)
-#     self.assertEqual(mock_call.call_count, 2)
-#     self.assertTrue(status)
 
-#     # cpid not exists
-#     mock_method = MagicMock(name='method')
-#     mock_pipe.side_effect = [('rfid1', 'wfid1'), ('rfid2', 'wfid2')]
-#     mock_fork.return_value = None
-#     mock_ush.return_value = None
-#     mock_close.side_effect = [None, None]
-#     mock_read.return_value = None
-#     mock_setgid.return_value = None
-#     moc_setuid.return_value = None
-#     mock_setgrp.return_value = None
-#     mock_exit.return_value = 1
-#     # status = Unshare().namespace_exec(mock_method)
-#     # self.assertFalse(status)
+def test_05_namespace_exec(mocker):
+    """Test05 Unshare().namespace_exec cpid = 0, Raises"""
+    mock_method = MagicMock(name='method')
+    mock_pipe = mocker.patch('os.pipe', side_effect=[('pread1', 'pwrite1'), ('pread1', 'pwrite1')])
+    mock_fork = mocker.patch('os.fork', return_value=0)
+    mock_unshare = mocker.patch.object(Unshare, 'unshare', return_value=True)
+    mock_close = mocker.patch('os.close', side_effect=[None, None])
+    mock_read = mocker.patch('os.read')
+    mock_setgid = mocker.patch('os.setgid', side_effect=OSError)
+    mock_logerr = mocker.patch('udocker.LOG.error')
+    mock_sysexit = mocker.patch('sys.exit', return_value=1)
+
+    status = Unshare().namespace_exec(mock_method)
+    assert not status
+    assert mock_pipe.call_count == 2
+    mock_fork.assert_called()
+    mock_unshare.assert_called()
+    assert mock_close.call_count == 2
+    mock_read.assert_called()
+    mock_setgid.assert_called()
+    mock_logerr.assert_called()
+    mock_sysexit.assert_not_called()
+
+
+def test_06_namespace_exec(mocker):
+    """Test06 Unshare().namespace_exec cpid = 1 status 0"""
+    mock_method = MagicMock(name='method')
+    mock_pipe = mocker.patch('os.pipe', side_effect=[('pread1', 'pwrite1'), ('pread1', 'pwrite1')])
+    mock_fork = mocker.patch('os.fork', return_value=1)
+    mock_close = mocker.patch('os.close', side_effect=[None, None])
+    mock_read = mocker.patch('os.read')
+    mock_hinfo = mocker.patch('udocker.helper.unshare.HostInfo')
+    mock_hinfo.return_value.username.return_value = 'user'
+    mock_hinfo.uid = 1000
+    mock_nixauth = mocker.patch('udocker.helper.unshare.NixAuthentication')
+    mock_nixauth.return_value.user_in_subuid.return_value = [(1000, 1000)]
+    mock_nixauth.return_value.user_in_subgid.return_value = [(1000, 1000)]
+    mock_subcall = mocker.patch('subprocess.call', side_effect=[None, None])
+    mock_wait = mocker.patch('os.waitpid', return_value=(123, 0))
+
+    status = Unshare().namespace_exec(mock_method)
+    assert status
+    assert mock_pipe.call_count == 2
+    mock_fork.assert_called()
+    assert mock_close.call_count == 2
+    mock_read.assert_called()
+    mock_hinfo.return_value.username.assert_called()
+    mock_nixauth.return_value.user_in_subuid.assert_called()
+    mock_nixauth.return_value.user_in_subgid.assert_called()
+    assert mock_subcall.call_count == 2
+    mock_wait.assert_called()
+
+
+def test_07_namespace_exec(mocker):
+    """Test07 Unshare().namespace_exec cpid = 1 status 1"""
+    mock_method = MagicMock(name='method')
+    mock_pipe = mocker.patch('os.pipe', side_effect=[('pread1', 'pwrite1'), ('pread1', 'pwrite1')])
+    mock_fork = mocker.patch('os.fork', return_value=1)
+    mock_close = mocker.patch('os.close', side_effect=[None, None])
+    mock_read = mocker.patch('os.read')
+    mock_hinfo = mocker.patch('udocker.helper.unshare.HostInfo')
+    mock_hinfo.return_value.username.return_value = 'user'
+    mock_hinfo.uid = 1000
+    mock_nixauth = mocker.patch('udocker.helper.unshare.NixAuthentication')
+    mock_nixauth.return_value.user_in_subuid.return_value = [(1000, 1000)]
+    mock_nixauth.return_value.user_in_subgid.return_value = [(1000, 1000)]
+    mock_subcall = mocker.patch('subprocess.call', side_effect=[None, None])
+    mock_wait = mocker.patch('os.waitpid', return_value=(123, 1))
+    mock_logerr = mocker.patch('udocker.LOG.error')
+
+    status = Unshare().namespace_exec(mock_method)
+    assert not status
+    assert mock_pipe.call_count == 2
+    mock_fork.assert_called()
+    assert mock_close.call_count == 2
+    mock_read.assert_called()
+    mock_hinfo.return_value.username.assert_called()
+    mock_nixauth.return_value.user_in_subuid.assert_called()
+    mock_nixauth.return_value.user_in_subgid.assert_called()
+    assert mock_subcall.call_count == 2
+    mock_wait.assert_called()
+    mock_logerr.assert_called()
