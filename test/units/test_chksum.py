35af1d02eba431055007bec6ad91145a281d0ba8
mariojmdavid <mariojmdavid@gmail.com>
2023-02-13 09:11:02 +0000

    simplify test_chksum

--- test/units/test_chksum.py
+++ test/units/test_chksum.py
@@ -11,8 +11,7 @@ def test_01_hash256(mocker):
     mocked_file_data = mocker.mock_open(read_data=b'qwertyui\n')
     mocker.patch("builtins.open", mocked_file_data)
     sha256sum = "42e6d97f00bff046ae2dea5c6db56d866ae749fb7dc05319e08e5fbee31d851c"
-    cksum = ChkSUM()
-    ret_function = cksum.hash(mocked_file_data, "sha256")
+    ret_function = ChkSUM().hash(mocked_file_data, "sha256")
     assert ret_function == sha256sum
 
 
@@ -22,8 +21,7 @@ def test_02_hash512(mocker):
     mocker.patch("builtins.open", mocked_file_data)
     sha512sum =("2a260f37d0e522ecb43d93f23bfbbe88846da321f9eb1a9b552b74"
                 "d1fe356ef20506b06f1ab639e0e0b028e85404e0f6eee3deba3f7a2367de54af8a079ba823")
-    cksum = ChkSUM()
-    ret_function = cksum.hash(mocked_file_data, "sha512")
+    ret_function = ChkSUM().hash(mocked_file_data, "sha512")
     assert ret_function == sha512sum
 
 
@@ -31,6 +29,5 @@ def test_03_hash256_nofile():
     """Test03 ChkSUM().hash sha256 no file."""
     with patch("builtins.open", mock_open()) as mock_file:
         mock_file.side_effect = OSError()
-        cksum = ChkSUM()
-        ret_function = cksum.hash("some_file", "sha256")
+        ret_function = ChkSUM().hash("some_file", "sha256")
         assert ret_function == ""
