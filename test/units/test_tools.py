4b453633e273f81b1e573f79bb8a5ca485db3fae
mariojmdavid <mariojmdavid@gmail.com>
2023-09-08 11:36:59 +0100

    wip test tools

--- test/units/test_tools.py
+++ test/units/test_tools.py
@@ -16,6 +16,8 @@ MOD1 = {
         "tarball": "crun-x86_64.tgz",
         "version": "1.6",
         "arch": "x86_64",
+        "os": 'null',
+        "os_ver": 'null',
         "kernel_ver": "",
         "sha256sum": "4907b4005436686e453297f7e011ddb655c410429f711d787546f5ecceb03048",
         "installdir": "bin/",
@@ -38,6 +40,8 @@ MOD2 = {
         "tarball": "proot-x86_64-4_8_0.tgz",
         "version": "5.1.1",
         "arch": "x86_64",
+        "os": 'null',
+        "os_ver": 'null',
         "kernel_ver": ">=4.8.0",
         "sha256sum": "1a926182aa16cc7ba77e278851a62f33995e2515e01c7a99932bb6e7f853b0a9",
         "installdir": "bin/",
@@ -60,6 +64,8 @@ MOD3 = {
         "tarball": "patchelf-x86_64.tgz",
         "version": "0.9",
         "arch": "x86_64",
+        "os": 'null',
+        "os_ver": 'null',
         "kernel_ver": "",
         "sha256sum": "9acf2db637365bf10379bc3e51658a6f0d3303156457f7827b04d3f9f32f7e6a",
         "installdir": "bin/",
@@ -82,6 +88,8 @@ MOD4 = {
         "tarball": "libfakechroot-x86_64.tgz",
         "version": "2.18",
         "arch": "x86_64",
+        "os": 'null',
+        "os_ver": 'null',
         "kernel_ver": "",
         "sha256sum": "c4280e0e3b3fa5ba6d94be0d43a2379d9d4390f08c96adc83cf4ba6be939d757",
         "installdir": "lib/",
@@ -126,7 +134,7 @@ def cnf():
 
 @pytest.fixture
 def logger(mocker):
-    mock_log = mocker.patch('udocker.engine.nvidia.LOG')
+    mock_log = mocker.patch('udocker.tools.LOG')
     return mock_log
 
 
@@ -320,11 +328,11 @@ def test_10_verify_sha(mocker, utools, lmods, lsha, expected):
     assert mock_sha.call_count == 2
 
 
-data_dtar = ((True, True, 2, True),
-             (False, False, 0, False))
+data_downtar = ((True, True, 2, True),
+                (False, False, 0, False))
 
 
-@pytest.mark.parametrize('vsha,force,cgetfile,expected', data_dtar)
+@pytest.mark.parametrize('vsha,force,cgetfile,expected', data_downtar)
 def test_11_download_tarballs(mocker, utools, vsha, force, cgetfile, expected):
     """Test11 UdockerTools().download_tarballs()."""
     lmods = [MOD1, MOD2]
@@ -340,272 +348,80 @@ def test_11_download_tarballs(mocker, utools, vsha, force, cgetfile, expected):
     assert mock_getfile.call_count == cgetfile
 
 
-# @patch.object(UdockerTools, '_install')
-# @patch.object(UdockerTools, '_verify_version')
-# @patch.object(UdockerTools, '_get_file')
-# @patch.object(UdockerTools, '_get_mirrors')
-# @patch('udocker.tools.FileUtil.remove')
-# def test_13__install_logic(self, mock_furm, mock_getmirr, mock_getfile, mock_ver, mock_install):
-#     """Test13 UdockerTools()._install_logic()."""
-#     mock_furm.return_value = None
-#     mock_getmirr.return_value = "https://down.pt/udocker-1.2.7.tar.gz"
-#     mock_getfile.return_value = "udocker-1.2.7.tar.gz"
-#     mock_ver.return_value = (True, "1.2.7")
-#     mock_install.return_value = True
-#     utools = UdockerTools(self.local)
-#     status = utools._install_logic(False)
-#     self.assertTrue(status)
-#     self.assertTrue(mock_getmirr.called)
-#     self.assertTrue(mock_getfile.called)
-#     self.assertTrue(mock_ver.called)
-#     self.assertTrue(mock_install.called)
+data_deltar = (([1], 1, 0),
+               ([], 0, 1))
 
-#     mock_furm.return_value = None
-#     mock_getmirr.return_value = "https://down.pt/udocker-1.2.7.tar.gz"
-#     mock_getfile.return_value = "udocker-1.2.7.tar.gz"
-#     mock_ver.return_value = (False, "")
-#     mock_install.return_value = True
-#     utools = UdockerTools(self.local)
-#     status = utools._install_logic(True)
-#     self.assertTrue(status)
 
-#     mock_furm.return_value = None
-#     mock_getmirr.return_value = "https://down.pt/udocker-1.2.7.tar.gz"
-#     mock_getfile.return_value = "udocker-1.2.7.tar.gz"
-#     mock_ver.return_value = (False, "")
-#     mock_install.return_value = True
-#     utools = UdockerTools(self.local)
-#     status = utools._install_logic(False)
-#     self.assertFalse(status)
+@pytest.mark.parametrize('listuid,cselmod,cgetmeta', data_deltar)
+def test_12_delete_tarballs(mocker, utools, listuid, cselmod, cgetmeta):
+    """Test12 UdockerTools().delete_tarballs()."""
+    lmods = [MOD1, MOD2]
+    mock_selmod = mocker.patch.object(UdockerTools, '_select_modules', return_value=lmods)
+    mock_getmeta = mocker.patch.object(UdockerTools, 'get_metadata', return_value=lmods)
+    mock_pexists = mocker.patch('udocker.tools.os.path.exists', side_effect=[True, True])
+    mock_rm = mocker.patch('udocker.tools.os.remove', side_effect=[None, None])
+    resout = utools.delete_tarballs(listuid, 'moddir')
+    assert resout
+    assert mock_selmod.call_count == cselmod
+    assert mock_getmeta.call_count == cgetmeta
+    assert mock_pexists.call_count == 2
+    assert mock_rm.call_count == 2
 
-# @patch.object(UdockerTools, 'get_installinfo')
-# @patch.object(UdockerTools, '_install_logic')
-# @patch.object(UdockerTools, 'is_available')
-# def test_14_install(self, mock_isavail, mock_instlog, mock_getinfo):
-#     """Test14 UdockerTools().install()."""
-#     Config.conf['autoinstall'] = True
-#     Config.conf['tarball'] = "udocker-1.2.7.tar.gz"
-#     Config.conf['tarball_release'] = "1.2.7"
-#     Config.conf['installretry'] = 2
-#     mock_isavail.return_value = True
-#     utools = UdockerTools(self.local)
-#     status = utools.install(False)
-#     self.assertTrue(status)
-#     self.assertTrue(mock_isavail.called)
 
-#     Config.conf['autoinstall'] = False
-#     Config.conf['tarball'] = "udocker-1.2.7.tar.gz"
-#     Config.conf['tarball_release'] = "1.2.7"
-#     Config.conf['installretry'] = 2
-#     mock_isavail.return_value = False
-#     utools = UdockerTools(self.local)
-#     status = utools.install(False)
-#     self.assertEqual(status, None)
-#     self.assertFalse(utools._autoinstall)
+def test_13_delete_tarballs(mocker, utools):
+    """Test13 UdockerTools().delete_tarballs()."""
+    lmods = [MOD1]
+    mock_selmod = mocker.patch.object(UdockerTools, '_select_modules', return_value=lmods)
+    mock_getmeta = mocker.patch.object(UdockerTools, 'get_metadata', return_value=lmods)
+    mock_pexists = mocker.patch('udocker.tools.os.path.exists', return_value=True)
+    mock_rm = mocker.patch('udocker.tools.os.remove', side_effect=OSError)
+    resout = utools.delete_tarballs([1], 'moddir')
+    assert not resout
+    mock_selmod.assert_called()
+    mock_getmeta.assert_not_called()
+    mock_pexists.assert_called()
+    mock_rm.assert_called()
 
-#     Config.conf['autoinstall'] = False
-#     Config.conf['tarball'] = ""
-#     Config.conf['tarball_release'] = "1.2.7"
-#     Config.conf['installretry'] = 2
-#     mock_isavail.return_value = True
-#     utools = UdockerTools(self.local)
-#     status = utools.install(False)
-#     self.assertTrue(status)
-#     self.assertEqual(utools._tarball, "")
 
-#     Config.conf['autoinstall'] = True
-#     Config.conf['tarball'] = "udocker-1.2.7.tar.gz"
-#     Config.conf['tarball_release'] = "1.2.7"
-#     Config.conf['installretry'] = 2
-#     mock_isavail.return_value = False
-#     mock_instlog.side_effect = [False, True]
-#     mock_getinfo.side_effect = [None, None]
-#     utools = UdockerTools(self.local)
-#     status = utools.install(False)
-#     self.assertTrue(status)
-#     self.assertTrue(mock_instlog.call_count, 2)
-#     self.assertTrue(mock_getinfo.call_count, 2)
+data_downlic = ((False, [False, True], 0, True),
+                (True, [True, False], 1, True),
+                (True, [False, False], 1, False))
 
-#     Config.conf['autoinstall'] = True
-#     Config.conf['tarball'] = "udocker-1.2.7.tar.gz"
-#     Config.conf['tarball_release'] = "1.2.7"
-#     Config.conf['installretry'] = 2
-#     mock_isavail.return_value = False
-#     mock_instlog.side_effect = [False, False]
-#     mock_getinfo.side_effect = [None, None]
-#     utools = UdockerTools(self.local)
-#     status = utools.install(False)
-#     self.assertFalse(status)
 
-# @patch('udocker.tools.json.load')
-# @patch('udocker.tools.LOG')
-# @patch('udocker.tools.os.path.isfile')
-# @patch.object(UdockerTools, '_get_file')
-# @patch.object(UdockerTools, '_get_mirrors')
-# def test_15__get_metadata(self, mock_mirr, mock_file, mock_isfile, mock_log, mock_jload):
-#     """Test15 UdockerTools()._get_metadata()."""
-#     mjson = [{
-#         "module": "proot",
-#         "version": "5.1.0",
-#         "arch": "arm",
-#         "os": "Centos",
-#         "os_ver": "7",
-#         "kernel_ver": "4.7.0",
-#         "dependencies": ["udocker"],
-#         "digit_signature": "",
-#         "uid": "12345",
-#         "fname": "proot-arm",
-#         "sha256sum": "1234567",
-#         "urls": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/bin/proot-arm"
-#         ],
-#         "docs_url": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/doc/proot_docs.tgz"
-#         ]
-#     }]
-#     mock_mirr.return_value = "https://download.ncg.ingrid.pt/webdav/udocker/metadata.json"
-#     mock_file.return_value = "metadata.json"
-#     mock_jload.return_value = mjson
-#     mock_isfile.return_value = False
-#     fjson_line = StringIO(str(mjson))
-#     with patch(BOPEN) as mopen:
-#         mopen.return_value = fjson_line
-#         utools = UdockerTools(self.local)
-#         status = utools._get_metadata(False)
-#         self.assertTrue(mock_log.info.called)
-#         self.assertFalse(mock_log.error.called)
+@pytest.mark.parametrize('force,mgetfile,cgetfile,expected', data_downlic)
+def test_14_download_licenses(mocker, utools, force, mgetfile, cgetfile, expected):
+    """Test14 UdockerTools().download_licenses()."""
+    lmods = [MOD1]
+    mock_selmod = mocker.patch.object(UdockerTools, '_select_modules', return_value=lmods)
+    mock_getfile = mocker.patch.object(UdockerTools, '_get_file', side_effect=mgetfile)
+    mock_isfile = mocker.patch('udocker.tools.os.path.isfile', side_effect=[True, True])
+    resout = utools.download_licenses('dstdir', 'srcloc', force)
+    assert resout == expected
+    mock_selmod.assert_called()
+    assert mock_getfile.call_count == cgetfile
 
-#     mock_jload.side_effect = KeyError("fail")
-#     utools = UdockerTools(self.local)
-#     status = utools._get_metadata(False)
-#     self.assertTrue(mock_log.error.called)
 
-# @patch('udocker.tools.LOG')
-# def test_16__match_mod(self, mock_log):
-#     """Test16 UdockerTools()._match_mod()."""
-#     mjson = [{
-#         "module": "proot",
-#         "version": "5.1.0",
-#         "arch": "arm",
-#         "os": "Centos",
-#         "os_ver": "7",
-#         "kernel_ver": "4.7.0",
-#         "dependencies": ["udocker"],
-#         "digit_signature": "",
-#         "uid": "12345",
-#         "fname": "proot-arm",
-#         "sha256sum": "1234567",
-#         "urls": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/bin/proot-arm"
-#         ],
-#         "docs_url": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/doc/proot_docs.tgz"
-#         ]
-#     }]
-#     utools = UdockerTools(self.local)
-#     status = utools._match_mod("runc", "x86_64", "Ubuntu", "16.04", mjson)
-#     self.assertFalse(mock_log.debug.called)
-#     self.assertEqual(status, [])
+data_showmeta = ((True, 18), (False, 2))
 
-#     utools = UdockerTools(self.local)
-#     status = utools._match_mod("proot", "arm", "Centos", "7", mjson)
-#     self.assertTrue(mock_log.debug.called)
-#     self.assertEqual(status, mjson[0]["urls"])
 
-# @patch('udocker.tools.LOG')
-# @patch('udocker.tools.OSInfo')
-# @patch.object(UdockerTools, '_match_mod')
-# @patch.object(UdockerTools, '_get_metadata')
-# def test_17_select_tarnames(self, mock_meta, mock_match, mock_osinfo, mock_log):
-#     """Test17 UdockerTools().select_tarnames()."""
-#     mjson = [{
-#         "module": "proot",
-#         "version": "5.1.0",
-#         "arch": "arm",
-#         "os": "Centos",
-#         "os_ver": "7",
-#         "kernel_ver": "4.7.0",
-#         "dependencies": ["udocker"],
-#         "digit_signature": "",
-#         "uid": "12345",
-#         "fname": "proot-arm",
-#         "sha256sum": "1234567",
-#         "urls": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/bin/proot-arm"
-#         ],
-#         "docs_url": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/doc/proot_docs.tgz"
-#         ]
-#     },
-#     {
-#         "module": "libfakechroot",
-#         "version": "4.3.0",
-#         "arch": "arm",
-#         "os": "Centos",
-#         "os_ver": "7",
-#         "kernel_ver": "4.7.0",
-#         "dependencies": ["udocker"],
-#         "digit_signature": "",
-#         "uid": "542433",
-#         "fname": "libfakechroot-arm.so",
-#         "sha256sum": "7483r932",
-#         "urls": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/lib/libfakechroot-arm.so"
-#         ],
-#         "docs_url": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/doc/proot_docs.tgz"
-#         ]
-#     }]
-#     res = ["https://download.ncg.ingrid.pt/webdav/udocker/engines/bin/proot-arm",
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/lib/libfakechroot-arm.so"]
-#     mock_meta.return_value = mjson
-#     utools = UdockerTools(self.local)
-#     status = utools.select_tarnames(["12345"])
-#     self.assertTrue(mock_log.debug.called)
-#     self.assertEqual(status, [["https://download.ncg.ingrid.pt/webdav/udocker/engines/bin/proot-arm"]])
+@pytest.mark.parametrize('long,cmsg', data_showmeta)
+def test_15_show_metadata(mocker, utools, long, cmsg):
+    """Test15 UdockerTools().show_metadata()."""
+    mock_msg = mocker.patch('udocker.tools.MSG.info')
+    assert utools.show_metadata([MOD1], long)
+    assert mock_msg.call_count == cmsg
 
-#     mock_meta.return_value = mjson
-#     mock_match.return_value = mjson[0]["urls"]
-#     mock_osinfo.return_value.arch.return_value = "amd64"
-#     mock_osinfo.return_value.osdistribution.return_value = ("linux", "4.8.1")
-#     utools = UdockerTools(self.local)
-#     status = utools.select_tarnames([])
-#     self.assertTrue(mock_log.debug.called)
-#     self.assertTrue(mock_osinfo.return_value.arch.called)
-#     self.assertTrue(mock_osinfo.return_value.osdistribution.called)
-# #        self.assertEqual(status, [res])
 
-# @patch('udocker.tools.MSG')
-# @patch.object(UdockerTools, '_get_metadata')
-# def test_18_show_metadata(self, mock_meta, mock_msg):
-#     """Test18 UdockerTools().show_metadata()."""
-#     mjson = [{
-#         "module": "proot",
-#         "version": "5.1.0",
-#         "arch": "arm",
-#         "os": "Centos",
-#         "os_ver": "7",
-#         "kernel_ver": "4.7.0",
-#         "dependencies": ["udocker"],
-#         "digit_signature": "",
-#         "uid": "12345",
-#         "fname": "proot-arm",
-#         "sha256sum": "1234567",
-#         "urls": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/bin/proot-arm"
-#         ],
-#         "docs_url": [
-#             "https://download.ncg.ingrid.pt/webdav/udocker/engines/doc/proot_docs.tgz"
-#         ]
-#     }]
-#     mock_meta.return_value = mjson
-#     utools = UdockerTools(self.local)
-#     status = utools.show_metadata(False)
-#     self.assertTrue(status)
-#     self.assertTrue(mock_msg.info.called)
+data_instlog = (([1], 1, 0),
+               ([], 0, 1))
 
-#     mock_meta.return_value = []
-#     utools = UdockerTools(self.local)
-#     status = utools.show_metadata(False)
-#     self.assertFalse(status)
-#     self.assertTrue(mock_msg.info.called)
+
+@pytest.mark.parametrize('lmods,force,expected', data_instlog)
+def test_16__installmod_logic(mocker, utools, lmods, force, expected):
+    """Test16 UdockerTools()._installmod_logic()."""
+    mock_selmod = mocker.patch.object(UdockerTools, '_select_modules', return_value=lmods)
+    mock_futil = mocker.patch('udocker.tools.FileUtil.rchmod', side_effect=[None, None])
+
+    resout = utools._installmod_logic([1], 'top_dir', 'tar_dir', force)
+    assert resout
+
