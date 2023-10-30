bdcc6d6e620703320d9a621757fa4fb8d183b483
jprm <jprm@lip.pt>
2023-10-30 17:30:24 +0000

    refactoring and removal of fixtures in test_singularity

--- test/units/test_singularity.py
+++ test/units/test_singularity.py
@@ -3,6 +3,7 @@
 udocker unit tests: SingularityEngine
 """
 import logging
+import os
 import random
 from contextlib import nullcontext as does_not_raise
 
@@ -11,6 +12,9 @@ import pytest
 
 from udocker.config import Config
 from udocker.engine.singularity import SingularityEngine
+from udocker.helper.hostinfo import HostInfo
+from udocker.helper.nixauth import NixAuthentication
+from udocker.utils.fileutil import FileUtil
 
 
 @pytest.fixture
@@ -32,33 +36,8 @@ def singularity(lrepo):
 
 
 @pytest.fixture
-def mock_os_path_exists(mocker):
-    return mocker.patch('udocker.engine.singularity.os.path.exists')
-
-
-@pytest.fixture
-def mock_os_path_isdir(mocker):
-    return mocker.patch('udocker.engine.singularity.os.path.isdir')
-
-
-@pytest.fixture
-def mock_fileutil(mocker):
-    return mocker.patch('udocker.engine.singularity.FileUtil')
-
-
-@pytest.fixture
 def logger(mocker):
     return mocker.patch('udocker.engine.singularity.LOG')
-
-
-@pytest.fixture
-def mock_hostinfo(mocker):
-    return mocker.patch('udocker.engine.singularity.HostInfo')
-
-
-@pytest.fixture
-def mock_nixauth(mocker):
-    return mocker.patch('udocker.engine.singularity.NixAuthentication')
 
 
 def test_01_init(singularity, tmpdir):
@@ -75,20 +54,22 @@ def test_01_init(singularity, tmpdir):
     (False, "UDOCKER", "", "i386", 1, "Error: singularity executable not found", pytest.raises(SystemExit),
      "singularity-x86_64"),
 ])
-def test_02_select_singularity(mocker, singularity, mock_fileutil, mock_os_path_exists, logger, os_exists,
-                               executable, findexec, arch, logcount, loggermsg, error, expected):
+def test_02_select_singularity(mocker, singularity, logger, os_exists, executable, findexec, arch, logcount, loggermsg,
+                               error, expected):
     """Test the select_singularity() function"""
-    mocker.patch.object(Config, 'conf', {'use_singularity_executable': executable})
+    mocker.patch.object(Config, 'conf', {'use_singularity_executable': executable, 'tmpdir': '/tmp'})
 
-    mock_os_path_exists.return_value = os_exists
-    mock_fileutil.return_value.find_exec.return_value = findexec
-    mock_fileutil.return_value.find_file_in_dir.return_value = findexec
+    mocker.patch.object(os.path, 'exists', return_value=os_exists)
+    mocker.patch.object(FileUtil, 'find_exec', return_value=findexec)
+    mocker.patch.object(FileUtil, 'find_file_in_dir', return_value=findexec)
 
     with error as exit_code:
         singularity.select_singularity()
+
         if exit_code is not None:
             assert exit_code.type == SystemExit
             assert exit_code.value.code == 1
+
         assert logger.error.call_args_list == loggermsg
         assert logger.error.call_count == logcount
         assert singularity.executable == findexec
@@ -106,14 +87,14 @@ def test_02_select_singularity(mocker, singularity, mock_fileutil, mock_os_path_
                                       '-B', '/var/tmp:/var/tmp']),
     ([], "/home/user", True, ['--home', '/root:/root', '-B', '/tmp:/tmp', '-B', '/var/tmp:/var/tmp']),
 ])
-def test_03__get_volume_bindings(mocker, singularity, mock_nixauth, mock_os_path_isdir, vol, home, istmp, expected):
+def test_03__get_volume_bindings(mocker, singularity, vol, home, istmp, expected):
     """Test the _get_volume_bindings() function"""
-    mock_os_path_isdir.side_effect = [istmp for _ in vol]
+    mocker.patch.object(os.path, 'isdir', side_effect=[istmp for _ in vol])
     mocker.patch.object(singularity, 'opt', {'vol': vol})
-    mock_nixauth.return_value.get_home.return_value = home
+    mocker.patch.object(NixAuthentication, 'get_home', return_value=home)
 
-    status = singularity._get_volume_bindings()
-    assert status == expected
+    get_volume_bindings = singularity._get_volume_bindings()
+    assert get_volume_bindings == expected
 
 
 @pytest.mark.parametrize("envkeys,expected", (
@@ -123,20 +104,19 @@ def test_03__get_volume_bindings(mocker, singularity, mock_nixauth, mock_os_path
 def test_04__singularity_env_get(mocker, singularity, envkeys, expected):
     """Test04 SingularityEngine()._singularity_env_get()."""
     mocker.patch.object(singularity, 'opt', {'env': envkeys})
-
-    status = singularity._singularity_env_get()
-    assert status == expected
+    singularity_env_get = singularity._singularity_env_get()
+    assert singularity_env_get == expected
 
 
 @pytest.mark.parametrize("mkdir", [
     ([True, True, True, True, True, True]),
     ([False, False, False, False, False, False]),
 ])
-def test_05__make_container_dirs(singularity, mock_fileutil, mkdir):
+def test_05__make_container_dirs(mocker, singularity, mkdir):
     """Test05 SingularityEngine()._make_container_directories()."""
-    mock_fileutil.return_value.mkdir.side_effect = mkdir
+    mock_fileutil = mocker.patch.object(FileUtil, 'mkdir', side_effect=mkdir)
     singularity._make_container_directories()
-    assert mock_fileutil.return_value.mkdir.call_count == 6
+    assert mock_fileutil.call_count == 6
 
 
 @pytest.mark.parametrize("portsmap,netcoop,logcount,call", [
@@ -161,18 +141,17 @@ def test_06__run_invalid_options(mocker, singularity, logger, portsmap, netcoop,
     ({'user': 'root', 'uid': 1001, 'home': "/home/user"}, "u1", 0, [], "", (False, False), False),
     ({'uid': 1001, 'home': "/home/user"}, "u1", 0, [], "", (False, False), False),
 ])
-def test_07__run_as_root(mocker, singularity, mock_hostinfo, mock_nixauth, logger, logcount, uname,
-                         loggermsg, option, user_in, expected, opts):
+def test_07__run_as_root(mocker, singularity, logger, logcount, uname, loggermsg, option, user_in, expected, opts):
     """Test07 SingularityEngine()._run_as_root()."""
     mocker.patch.object(Config, 'conf', {'singularity_options': []})
     mocker.patch.object(singularity, 'opt', opts)
     mocker.patch.object(singularity, '_has_option', return_value=lambda x, y: True if option == "--fakeroot" else False)
-
-    mock_hostinfo.return_value.username.return_value = uname
-    mock_nixauth.return_value.user_in_subuid.return_value = user_in[0]
-    mock_nixauth.return_value.user_in_subgid.return_value = user_in[1]
+    mocker.patch.object(HostInfo, 'username', return_value=uname)
+    mocker.patch.object(NixAuthentication, 'user_in_subuid', return_value=user_in[0])
+    mocker.patch.object(NixAuthentication, 'user_in_subgid', return_value=user_in[1])
 
     assert singularity._run_as_root() == expected
+
     assert ('--fakeroot' in Config.conf['singularity_options']) == expected
     assert logger.warning.call_count == logcount
     assert logger.warning.call_args_list == loggermsg
@@ -189,8 +168,7 @@ def test_07__run_as_root(mocker, singularity, mock_hostinfo, mock_nixauth, logge
      [False, False], '/home/user/execpath', 0),
     (False, None, None, {}, [False, False], '', 2),
 ])
-def test_08_run(mocker, singularity, container_id, mock_nixauth, is_dir, execpath, loglevel, opts, has_option,
-                executable, expected):
+def test_08_run(mocker, singularity, container_id, is_dir, execpath, loglevel, opts, has_option, executable, expected):
     """Test08 SingularityEngine().run()."""
     mocker.patch.object(singularity, 'opt', opts)
 
@@ -213,9 +191,7 @@ def test_08_run(mocker, singularity, container_id, mock_nixauth, is_dir, execpat
                                                   'sysdirs_list': (
                                                       "/etc/resolv.conf", "/etc/host.conf", "/lib/modules",)})
 
-    if loglevel:
-        config.update(loglevel)
-
+    config.update(loglevel) if loglevel else None
     mocker.patch.object(singularity, '_run_init', lambda x: execpath)
 
     with mocker.patch('subprocess.call', return_value=0):
