9f7a89e637532acf49810cb669d80a7d21844460
jprm <jprm@lip.pt>
2023-10-31 15:45:55 +0000

    fix test_08_run, adds test_07__get_qemu_string, remove fixtures, refactoring and fix code style

--- test/units/test_proot.py
+++ test/units/test_proot.py
@@ -2,8 +2,10 @@
 """
 udocker unit tests: PRootEngine
 """
+import logging
 import os
 import random
+import subprocess
 from contextlib import nullcontext as does_not_raise
 
 import pytest
@@ -11,6 +13,10 @@ import pytest
 from udocker.config import Config
 from udocker.engine.execmode import ExecutionMode
 from udocker.engine.proot import PRootEngine
+from udocker.helper.hostinfo import HostInfo
+from udocker.utils.fileutil import FileUtil
+from udocker.utils.uenv import Uenv
+from udocker.utils.uprocess import Uprocess
 
 KERNEL_VERSIONS = ["4.8.13", "4.8.0", "4.5.0"]
 EXECUTABLE = ["UDOCKER", "proot", "proot-x86_64-4_8_13", "proot-x86_64-4_8_0", "proot-x86_64-4_5_0"]
@@ -28,11 +34,6 @@ def localrepo(mocker, container_id):
     mocker_localrepo._container_id = container_id
     mocker_localrepo.bindir = "proot"
     return mocker_localrepo
-
-
-@pytest.fixture
-def mock_hostinfo(mocker):
-    return mocker.patch('udocker.engine.proot.HostInfo')
 
 
 @pytest.fixture(autouse=True)
@@ -55,25 +56,10 @@ def proot(localrepo, mock_exec_mode):
     return mocker_proot
 
 
-@pytest.fixture(autouse=True)
-def mock_os_exists(mocker):
-    return mocker.patch('udocker.engine.proot.os.path.exists')
-
-
 @pytest.fixture
 def logger(mocker):
     mocker_logger = mocker.patch('udocker.engine.proot.LOG')
     return mocker_logger
-
-
-@pytest.fixture
-def mock_fileutil(mocker):
-    return mocker.patch('udocker.engine.proot.FileUtil')
-
-
-@pytest.fixture
-def mock_get_output(mocker):
-    return mocker.patch('udocker.engine.proot.Uprocess.get_output')
 
 
 @pytest.mark.parametrize('kernel', KERNEL_VERSIONS)
@@ -86,11 +72,12 @@ def test_01_init(proot, kernel):
     assert proot._kernel == kernel
 
 
-@pytest.mark.parametrize('is_greater', (True, False))
-@pytest.mark.parametrize('pathexists,logcount,msg,error,conf,pathexec,kernel,is_seccomp_patched,expected', [
-    (True, 0, "", does_not_raise(), {'use_proot_executable': 'UDOCKER', 'proot_noseccomp': None}, "/some/path/proot",
+@pytest.mark.parametrize('pathexists, logcount, msg,error, conf, pathexec, kernel, is_seccomp_patched, expected', [
+    (True, 0, "", does_not_raise(), {'use_proot_executable': 'UDOCKER', 'proot_noseccomp': None, 'tmpdir': 'tmp'},
+     "/some/path/proot",
      KERNEL_VERSIONS, False, ("/some/path/proot", False)),
-    (True, 0, "", does_not_raise(), {'use_proot_executable': None, 'proot_noseccomp': None}, "/some/path/proot",
+    (True, 0, "", does_not_raise(), {'use_proot_executable': None, 'proot_noseccomp': None, 'tmpdir': 'tmp'},
+     "/some/path/proot",
      KERNEL_VERSIONS, False, ('/some/path/proot', False)),
     (True, 0, "", does_not_raise(), {'use_proot_executable': "PROOT", 'proot_noseccomp': None}, "/some/path/proot",
      KERNEL_VERSIONS, True, ('PROOT', False)),
@@ -101,20 +88,17 @@ def test_01_init(proot, kernel):
     (True, 0, "", does_not_raise(), {'use_proot_executable': "PROOT", 'proot_noseccomp': True},
      "/some/path/proot", KERNEL_VERSIONS, True, ('PROOT', True)),
 ])
+@pytest.mark.parametrize('is_greater', (True, False))
 @pytest.mark.parametrize('xmode', XMODE)
-def test_02_select_proot(mocker, proot, mock_os_exists, mock_hostinfo, mock_fileutil, is_greater,
-                         pathexists, is_seccomp_patched,
-                         logger, logcount, msg, error, conf, pathexec, xmode, kernel, expected):
+def test_02_select_proot(mocker, proot, logger, is_greater, xmode, pathexists, logcount, msg, error, conf, pathexec,
+                         kernel, is_seccomp_patched, expected):
     """Test02 PRootEngine().select_proot()."""
-
     mocker.patch.object(Config, 'conf', conf)
-
-    mock_os_exists.return_value = pathexists
-    mock_fileutil.return_value.find_file_in_dir.return_value = pathexec
-    mock_hostinfo.return_value.arch.return_value = 'amd64'
-    mock_hostinfo.return_value.oskernel_isgreater.return_value = is_greater
-    mock_fileutil.return_value.find_exec.return_value = pathexec
-
+    mocker.patch.object(os.path, 'exists', return_value=pathexists)
+    mocker.patch.object(FileUtil, 'find_file_in_dir', return_value=pathexec)
+    mocker.patch.object(HostInfo, 'arch', return_value='amd64')
+    mocker.patch.object(HostInfo, 'oskernel_isgreater', return_value=is_greater)
+    mocker.patch.object(FileUtil, 'find_exec', return_value=pathexec)
     mocker.patch.object(proot, '_is_seccomp_patched', return_value=is_seccomp_patched, autospec=True)
 
     with error:
@@ -125,8 +109,7 @@ def test_02_select_proot(mocker, proot, mock_os_exists, mock_hostinfo, mock_file
         assert logger.error.called == logcount
 
 
-@pytest.mark.parametrize('xmode', XMODE)
-@pytest.mark.parametrize('os_env,saved_os,kernel,output,expected', [
+@pytest.mark.parametrize('os_env, saved_os, kernel, output, expected', [
     ({'PROOT_NEW_SECCOMP': '1'}, {}, "4.8.0", [], True),
     ({'PROOT_NO_SECCOMP': '1'}, {}, "4.8.0", [], False),
     (None, {}, "4.8.0", [], False),
@@ -136,103 +119,132 @@ def test_02_select_proot(mocker, proot, mock_os_exists, mock_hostinfo, mock_file
     (None, {}, "4.1.0", ["proot", ""], False),
     (None, {}, "4.1.0", ["", "proot"], True),
 ])
-def test_03__is_seccomp_patched(mocker, proot, mock_get_output,
-                                expected, os_env, saved_os, kernel, output):
+@pytest.mark.parametrize('xmode', XMODE)
+def test_03__is_seccomp_patched(mocker, proot, os_env, saved_os, kernel, output, expected):
     """Test03 PRootEngine()._is_seccomp_patched()."""
-    mock_get_output.side_effect = output
+    mocker.patch.object(Uprocess, 'get_output', side_effect=output, autospec=True)
     mocker.patch.object(proot, '_get_saved_osenv', return_value=saved_os, autospec=True)
+    mocker.patch.dict(os.environ, os_env) if os_env else None
 
-    if os_env:
-        mocker.patch.dict(os.environ, os_env)
+    is_seccomp_patched = proot._is_seccomp_patched("proot")
 
-    assert proot._is_seccomp_patched("/bin/sec") == expected
+    assert is_seccomp_patched == expected
 
 
-@pytest.mark.parametrize('xmode', ['P1', 'P2'])
-@pytest.mark.parametrize('kernel', ['4.8.13'])
-@pytest.mark.parametrize('uid,gid,expected', [
+@pytest.mark.parametrize('uid, gid, expected', [
     ("0", "1001", ['-0']),
     ("1001", "0", ['-i', '1001:0']),
     ("1000", "1001", ['-i', '1000:1001']),
 ])
-def test_03__set_uid_map(proot, uid, gid, expected):
+@pytest.mark.parametrize('xmode', ['P1', 'P2'])
+@pytest.mark.parametrize('kernel', ['4.8.13'])
+def test_03__set_uid_map(mocker, proot, uid, gid, expected):
     """Test03 PRootEngine()._set_uid_map()."""
-    proot.opt['uid'] = uid
-    proot.opt["gid"] = gid
-    assert proot._set_uid_map() == expected
+    mocker.patch.object(proot, 'opt', {'uid': uid, 'gid': gid})
+
+    set_uid_map = proot._set_uid_map()
+
+    assert set_uid_map == expected
 
 
 @pytest.mark.parametrize('kernel', KERNEL_VERSIONS)
 @pytest.mark.parametrize('xmode', ['P1', 'P2'])
 def test_04__create_mountpoint(proot):
     """Test04 PRootEngine()._create_mountpoint()."""
-    assert proot._create_mountpoint("", "") is True
+    create_mountpoint = proot._create_mountpoint("", "")
+
+    assert create_mountpoint is True
 
 
-@pytest.mark.parametrize('xmode', ['P1'])
-@pytest.mark.parametrize('kernel', ['4.8.13'])
-@pytest.mark.parametrize("vol,expected", [
+@pytest.mark.parametrize("vol, expected", [
     ((), []),
     (("/tmp", "/bbb",), ['-b', '/tmp:/tmp', '-b', '/bbb:/bbb']),
     (("/tmp",), ['-b', '/tmp:/tmp']),
 ])
+@pytest.mark.parametrize('xmode', ['P1'])
+@pytest.mark.parametrize('kernel', ['4.8.13'])
 def test_05__get_volume_bindings(mocker, proot, vol, expected):
     """Test05 PRootEngine()._get_volume_bindings()."""
     mocker.patch.object(proot, 'opt', {'vol': vol})
-    assert proot._get_volume_bindings() == expected
+
+    get_volume_bindings = proot._get_volume_bindings()
+
+    assert get_volume_bindings == expected
 
 
-@pytest.mark.parametrize('xmode', ['P1'])
-@pytest.mark.parametrize('kernel', ['4.8.13'])
-@pytest.mark.parametrize("netcoop,portsmap,expected", [
+@pytest.mark.parametrize("netcoop, portsmap, expected", [
     (None, {}, []),
     (None, {80: 8080}, ["-p", "80:8080"]),
     (None, {80: 8080, 443: 8443}, ["-p", "80:8080", "-p", "443:8443"]),
     (True, {80: 8080, 443: 8443, 53: 53}, ["-p", "80:8080", "-p", "443:8443", "-p", "53:53", "-n"]),
 ])
+@pytest.mark.parametrize('xmode', ['P1'])
+@pytest.mark.parametrize('kernel', ['4.8.13'])
 def test_06__get_network_map(mocker, proot, netcoop, portsmap, expected):
     """Test06 PRootEngine()._get_network_map()."""
     mocker.patch.object(proot, 'opt', {'netcoop': netcoop})
     mocker.patch.object(proot, '_get_portsmap', return_value=portsmap)
+
     assert proot._get_network_map() == expected
 
-# FIXME: this test comment since needs change in the src code to pass
 
-# @pytest.mark.parametrize('xmode', XMODE)
-# @pytest.mark.parametrize('kernel', ['4.8.13'])
-# @pytest.mark.parametrize('run_init,env_keys,opt_dict,oskernel_isgreater,loglevel,expected', [
-#     (False, {}, {'cmd': [''], 'cwd': [], 'env': {}}, False, {'verbose_level': logging.DEBUG}, 2),
-#     (True, {"PROOT_NO_SECCOMP": 1, "PROOT_NEW_SECCOMP": 1}, {'cmd': 'cmd', 'cwd': 'pwd', 'env': {}},
-#      {'verbose_level': logging.DEBUG}, True, 1),
-#     (True, {}, {'cmd': [''], 'cwd': [], 'env': {}}, False, {}, 1),
-# ])
-# def test_07_run(mocker, proot, container_id, run_init, expected, kernel, opt_dict, oskernel_isgreater, env_keys,
-#                 loglevel):
-#     """Test07 PRootEngine().run()."""
-#
-#     opt_dict.update({'kernel': kernel})
-#     mocker.patch.object(proot, 'opt', opt_dict)
-#
-#     mocker.patch('udocker.engine.proot.HostInfo.oskernel_isgreater', return_value=oskernel_isgreater)
-#     mocker.patch('udocker.engine.proot.subprocess.call', return_value=expected)
-#     mocker.patch.dict('udocker.engine.proot.Config.conf', {'verbose_level': logging.DEBUG, 'proot_killonexit': False})
-#     mocker.patch('udocker.engine.proot.os.getenv', side_effect=lambda key: env_keys.get(key, None))
-#     mocker.patch('udocker.engine.proot.os.environ.update')
-#
-#     mocker.patch.object(proot, '_run_init', return_value=run_init)
-#     mocker.patch.object(proot, 'select_proot')
-#     mocker.patch.object(proot, '_run_env_set')
-#     mocker.patch.object(proot, '_set_cpu_affinity', return_value=[])
-#     mocker.patch.object(proot, '_get_qemu_string', return_value=[])
-#     mocker.patch.object(proot, '_get_volume_bindings', return_value=[])
-#     mocker.patch.object(proot, '_set_uid_map', return_value=[])
-#     mocker.patch.object(proot, '_get_network_map', return_value=[])
-#     mocker.patch.object(proot, '_run_banner', return_value=[])
-#     mocker.patch.object(proot, '_run_env_cleanup_dict', return_value=None)
-#
-#     with mocker.patch('subprocess.call', return_value=expected):
-#         status = proot.run(container_id=container_id)
-#         assert proot.opt.get('env').get('PROOT_NO_SECCOMP') == (1 if env_keys.get("PROOT_NO_SECCOMP") else None)
-#         assert proot.opt.get('env').get('PROOT_NEW_SECCOMP') == (1 if env_keys.get("PROOT_NEW_SECCOMP") else None)
-#         assert proot._kernel == kernel if oskernel_isgreater else "6.0.0"
-#         assert status == expected
+@pytest.mark.parametrize("qemu_return, expected", [
+    ("qemu-x86_64", ["-q", "qemu-x86_64"]),
+    (None, []),
+    ("", []),
+])
+@pytest.mark.parametrize('kernel', ['4.8.13'])
+@pytest.mark.parametrize('xmode', ['P1', 'P2'])
+def test_07__get_qemu_string(mocker, proot, qemu_return, kernel, xmode, expected):
+    mocker.patch.object(proot, '_get_qemu', return_value=qemu_return)
+
+    result = proot._get_qemu_string()
+
+    assert result == expected
+
+
+@pytest.mark.parametrize('run_init, env_keys, opt_dict, oskernel_isgreater, loglevel, proot_killonexit, expected', [
+    (False, {}, {'cmd': [''], 'cwd': [], 'env': {}}, False, logging.DEBUG, False, 2),
+    (True, {"PROOT_NO_SECCOMP": '1', "PROOT_NEW_SECCOMP": '1'}, {'cmd': 'cmd', 'cwd': 'pwd', 'env': {}},
+     True, logging.DEBUG, True, 1),
+    (True, {"PROOT_NO_SECCOMP": '1', "PROOT_NEW_SECCOMP": '1'}, {'cmd': 'cmd', 'cwd': 'pwd', 'env': {}},
+     True, logging.NOTSET, False, 1),
+    (True, {}, {'cmd': [''], 'cwd': [], 'env': {}}, False, logging.NOTSET, False, 1),
+])
+@pytest.mark.parametrize('kernel', ['4.8.13'])
+@pytest.mark.parametrize('xmode', XMODE)
+def test_08_run(mocker, proot, container_id, kernel, run_init, env_keys, opt_dict, oskernel_isgreater, loglevel,
+                proot_killonexit, expected):
+    """Test07 PRootEngine().run()."""
+    uenv_instance = Uenv()
+    for key, value in env_keys.items():
+        uenv_instance.append(f"{key}={value}")
+
+    mocker.patch.object(proot, 'opt', {
+        "env": uenv_instance,
+        "cmd": opt_dict['cmd'],
+        "cwd": opt_dict['cwd'],
+        'kernel': kernel
+    })
+
+    mocker.patch.object(HostInfo, 'oskernel_isgreater', return_value=oskernel_isgreater)
+    mocker.patch.object(subprocess, 'call', return_value=expected)
+    mocker.patch.object(Config, 'conf', {'verbose_level': loglevel, 'proot_killonexit': proot_killonexit})
+    mocker.patch.object(os, 'getenv', side_effect=lambda key: env_keys.get(key, None))
+    mocker.patch.object(proot, '_run_init', return_value=run_init)
+    mocker.patch.object(proot, 'select_proot')
+    mocker.patch.object(proot, '_run_env_set')
+    mocker.patch.object(proot, '_set_cpu_affinity', return_value=[])
+    mocker.patch.object(proot, '_get_qemu_string', return_value=[])
+    mocker.patch.object(proot, '_get_volume_bindings', return_value=[])
+    mocker.patch.object(proot, '_set_uid_map', return_value=[])
+    mocker.patch.object(proot, '_get_network_map', return_value=[])
+    mocker.patch.object(proot, '_run_banner', return_value=[])
+    mocker.patch.object(proot, '_run_env_cleanup_dict', return_value=None)
+    mocker.patch.object(proot, '_has_option', return_value=True)
+
+    with mocker.patch('subprocess.call', return_value=expected):
+        status = proot.run(container_id=container_id)
+        assert proot.opt.get('env').env == env_keys
+        assert proot._kernel == kernel if oskernel_isgreater else "6.0.0"
+        assert status == expected
