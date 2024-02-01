926c9c69de02f9471f8157403c3028a3129d2d8a
Yaroslav Halchenko <debian@onerussian.com>
2024-02-01 13:13:36 -0500

    [DATALAD RUNCMD] Do interactive fixing of typos
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "codespell -w -i 3 -C 2 ./udocker/genstr.py ./paper/paper.md",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- udocker/genstr.py
+++ udocker/genstr.py
@@ -4,7 +4,7 @@ import sys
 
 
 def is_genstr(objstr):
-    """test if objstr is string or unicode both in py2 nd py3
+    """test if objstr is string or unicode both in py2 and py3
     unicode type has been removed in py3
     :param objstr (string): object to test if string or unicode
     :return (bool) is_gstr if it is string or unicode or not
