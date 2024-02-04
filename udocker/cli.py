5bd12d273aca0e27d664eace6ebfccc7c72df927
Jorge Gomes <jorge@lip.pt>
2024-02-04 20:04:27 +0000

    remove experimental status of --nvidia

--- udocker/cli.py
+++ udocker/cli.py
@@ -1215,7 +1215,6 @@ class UdockerCLI(object):
         --purge                 :clean mountpoints and files created by udocker
         --fixperm               :attempt to fix file permissions
         --nvidia                :add NVIDIA libraries and binaries
-                                 (nvidia support is EXPERIMENTAL)
 
         <mode> is one of the following execution modes:
         P1: proot accelerated mode using seccomp filtering (default)
