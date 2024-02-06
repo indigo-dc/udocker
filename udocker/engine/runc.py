c89afbe7c3d47ed4f844a07a6e7417137808a207
Jorge Gomes <jorge@lip.pt>
2024-02-06 11:36:33 +0000

    initial support for runsc

--- udocker/engine/runc.py
+++ udocker/engine/runc.py
@@ -44,12 +44,14 @@ class RuncEngine(ExecutionEngineCommon):
             self.executable = FileUtil("runc").find_exec()
         if not self.executable:
             self.executable = FileUtil("crun").find_exec()
+        if not self.executable:
+            self.executable = FileUtil("runsc").find_exec()
 
         arch = HostInfo().arch()
         if self.executable == "UDOCKER" or not self.executable:
             self.executable = ""
             image_list = []
-            eng = ["runc", "crun"]
+            eng = ["runc", "crun", "runsc"]
             image_list = [eng[0]+"-"+arch, eng[0], eng[1]+"-"+arch, eng[1]]
 
             f_util = FileUtil(self.localrepo.bindir)
@@ -68,6 +70,8 @@ class RuncEngine(ExecutionEngineCommon):
             self.engine_type = "crun"
         elif "runc" in os.path.basename(self.executable):
             self.engine_type = "runc"
+        elif "runsc" in os.path.basename(self.executable):
+            self.engine_type = "runsc"
 
     def _load_spec(self, new=False):
         """Generate runc spec file"""
@@ -76,7 +80,11 @@ class RuncEngine(ExecutionEngineCommon):
             FileUtil(self._container_specfile).remove()
 
         if FileUtil(self._container_specfile).size() == -1:
-            cmd_l = [self.executable, "spec", "--rootless", ]
+            if self.engine_type == "runsc":
+                cmd_l = [self.executable, "spec", ]
+            else:
+                cmd_l = [self.executable, "spec", "--rootless", ]
+
             status = subprocess.call(cmd_l, shell=False, stderr=Msg.chlderr,
                                      close_fds=True,
                                      cwd=os.path.realpath(self._container_specdir))
@@ -413,6 +421,9 @@ class RuncEngine(ExecutionEngineCommon):
         cmd_l = self._set_cpu_affinity()
         cmd_l.append(self.executable)
         cmd_l.extend(runc_debug)
+        if self.engine_type == "runsc":
+            cmd_l.extend(["--network", "host", "--ignore-cgroups"])
+            cmd_l.extend(["--rootless", ])
         cmd_l.extend(["--root", self._container_specdir, "run"])
         cmd_l.extend(["--bundle", self._container_specdir, self.execution_id])
         Msg().err("CMD =", cmd_l, l=Msg.VER)
