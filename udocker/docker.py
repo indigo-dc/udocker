e383532a6003da9246c8a4066bf6fb12b35ac9b2
Jorge Gomes <jorge@lip.pt>
2024-03-12 21:55:09 +0000

    fix handling of library in image name

--- udocker/docker.py
+++ udocker/docker.py
@@ -572,8 +572,11 @@ class DockerIoAPI(object):
         if '.' in components[0] and len(components) >= 2:
             registry = components[0]
             del components[0]
+        #if ('.' not in components[0] and
+        #        components[0] != "library" and len(components) == 1):
         if ('.' not in components[0] and
-                components[0] != "library" and len(components) == 1):
+                components[0] != "library" and len(components) == 1 and
+                ((not registry) or "docker.io" in registry)):
             components.insert(0, "library")
         remoterepo = '/'.join(components)
         if registry:
